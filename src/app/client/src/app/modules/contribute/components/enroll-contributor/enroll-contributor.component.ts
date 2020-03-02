88d9113cf4193d79d21f4f1aca52963e6cb687f2
Nitesh Kesarkar <nitesh_k@techjoomla.com>
2020-03-02 19:45:19 +0530

    Enroll as contributor modal popup added

--- src/app/client/src/app/modules/contribute/components/enroll-contributor/enroll-contributor.component.ts
+++ src/app/client/src/app/modules/contribute/components/enroll-contributor/enroll-contributor.component.ts
@@ -7,17 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnrollContributorComponent implements OnInit {
   contributorType = 'individual';
+  showModal = false;
   data;
-  nameOfOrgani;
   constructor() { }
 
   ngOnInit(): void {
-    this.nameOfOrgani= "Name of organi";
     this.data =
     {
-      'filteredOptions' :
-      [{'name': 'NCERT', 'value': 'NCERT'}, {'name': 'CBSE', 'value': 'CBSE'}, {'name': 'ICSE', 'value': 'ICSE'}, {'name': 'UP', 'value': 'UP'}]
+      'board' : [
+        {'name': 'NCERT', 'value': 'NCERT'}, 
+        {'name': 'CBSE', 'value': 'CBSE'}, 
+        {'name': 'ICSE', 'value': 'ICSE'}, 
+        {'name': 'UP', 'value': 'UP'}
+      ],
+      'medium' : [
+        {'name': 'English', 'value': 'English'}, 
+        {'name': 'Marathi', 'value': 'Marathi'}, 
+        {'name': 'Hindi', 'value': 'Hindi'}, 
+        {'name': 'Oriya', 'value': 'Oriya'}
+      ],
+      'grade' : [
+        {'name': 'Grade 1', 'value': 'Grade1'}, 
+        {'name': 'Grade 2', 'value': 'Grade2'}, 
+        {'name': 'Grade 3', 'value': 'Grade3'}, 
+        {'name': 'Grade 4', 'value': 'Grade4'}
+      ],
+      'subject' : [
+        {'name': 'Marathi', 'value': 'Marathi'}, 
+        {'name': 'Hindi', 'value': 'Hindi'}, 
+        {'name': 'English', 'value': 'English'}, 
+        {'name': 'Science', 'value': 'Science'}
+      ]
     }
+  }
+
+  handleSubmit() {
+    console.log("submit");
   }
 
 }
