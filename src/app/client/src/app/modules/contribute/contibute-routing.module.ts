dfd1f837e9a4b142f8cb15de67019d132a498fb9
nilesh5995 <nilesh_s@techjoomla.com>
2020-03-02 23:19:04 +0530

    saves the my prgramm

--- src/app/client/src/app/modules/contribute/contibute-routing.module.ts
+++ src/app/client/src/app/modules/contribute/contibute-routing.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProgramComponent } from './components';
 import { EnrollContributorComponent } from './components/enroll-contributor/enroll-contributor.component';
 import { ListAllMyProgramsComponent } from './components/list-all-my-programs/list-all-my-programs.component';
+import { MyProgramComponent} from './components/my-program/my-program.component';
 const routes: Routes = [{
   path: '', component: ListAllProgramsComponent, canActivate: [ProgramsService],
   data: {
@@ -21,15 +22,21 @@ const routes: Routes = [{
 },
 {
   path: 'myenrollprograms', component: ListAllMyProgramsComponent, pathMatch: 'full',
-  data: {
-    telemetry: { env: 'programs', type: 'view', subtype: 'paginate' }
-  }, 
+  // data: {
+  //   telemetry: { env: 'programs', type: 'view', subtype: 'paginate' }
+  // }, 
 },
+// {
+//   path: 'myprogram/:programId', component: ProgramComponent, pathMatch: 'full',
+//     data: {
+//       telemetry: { env: 'myprograms', type: 'view', subtype: 'paginate' }
+//     },
+// },
 {
-  path: 'myprogram/:programId', component: ProgramComponent, pathMatch: 'full',
-    data: {
-      telemetry: { env: 'myprograms', type: 'view', subtype: 'paginate' }
-    },
+  path: 'myprogram/:programId', component: MyProgramComponent, pathMatch: 'full',
+  // data: {
+  //   telemetry: { env: 'programs', type: 'view', subtype: 'paginate' }
+  // },
 },
 {
   path: 'program/:programId', component: ProgramComponent,
