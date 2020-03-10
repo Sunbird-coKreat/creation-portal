0b0a2534ef4cd448e12acc2cc882fcc963a17bdc
Nitesh Kesarkar <nitesh_k@techjoomla.com>
2020-03-10 22:11:27 +0530

    Added program details on the nominations list view

--- src/app/client/src/app/modules/program/components/list-nominations/list-nominations.component.ts
+++ src/app/client/src/app/modules/program/components/list-nominations/list-nominations.component.ts
@@ -1,8 +1,10 @@
-import { ResourceService, ConfigService, NavigationHelperService } from '@sunbird/shared';
-import { ProgramsService, PublicDataService } from '@sunbird/core';
+import { ResourceService, ConfigService, NavigationHelperService, ToasterService } from '@sunbird/shared';
+import { ProgramsService, PublicDataService, UserService, FrameworkService } from '@sunbird/core';
 import { Component, OnInit, AfterViewInit, Input, Output, EventEmitter} from '@angular/core';
 import * as _ from 'lodash-es';
+import { tap, first } from 'rxjs/operators';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ISessionContext } from '../../../cbse-program/interfaces';
 
 @Component({
   selector: 'app-list-nominations',
@@ -16,18 +18,66 @@ export class ListNominationsComponent implements OnInit, AfterViewInit {
 	 onApprove = new EventEmitter();
 	 @Output()
    onReject = new EventEmitter();
-   
+
+   public sessionContext: ISessionContext = {};
    show = false;
+   public programId: string;
+   public programDetails: any;
+   public userProfile: any;
 
-  constructor(private programsService: ProgramsService, public resourceService: ResourceService,
+  constructor(public frameworkService: FrameworkService, private programsService: ProgramsService, public resourceService: ResourceService,
     private config: ConfigService, private publicDataService: PublicDataService,
-    private activatedRoute: ActivatedRoute, private router: Router, private navigationHelperService: NavigationHelperService) { }
-	
-  ngOnInit() {
+    private activatedRoute: ActivatedRoute, private router: Router, private navigationHelperService: NavigationHelperService, public toasterService: ToasterService, public userService: UserService)  {
+      this.programId = this.activatedRoute.snapshot.params.programId;
   }
-
+	
   ngAfterViewInit() {
   }
+
+  ngOnInit() {
+    this.getProgramDetails();
+  }
+
+  getProgramDetails() {
+    this.fetchProgramDetails().subscribe((programDetails) => {
+      this.programDetails = _.get(programDetails, 'result');
+    }, error => {
+      // TODO: navigate to program list page
+      const errorMes = typeof _.get(error, 'error.params.errmsg') === 'string' && _.get(error, 'error.params.errmsg');
+      this.toasterService.error(errorMes || 'Fetching program details failed');
+    });
+  }
+
+  fetchProgramDetails() {
+    const req = {
+      url: `/program/v1/read/${this.programId}`
+    };
+    return this.programsService.get(req).pipe(tap((programDetails: any) => {
+      programDetails.result.config = JSON.parse(programDetails.result.config);
+      this.programDetails = programDetails.result;
+      this.sessionContext.framework = _.get(this.programDetails, 'config.framework');
+      if (this.sessionContext.framework) {
+        this.userProfile = this.userService.userProfile;
+        this.fetchFrameWorkDetails();
+      }
+    }));
+  }
+
+  public fetchFrameWorkDetails() {
+    this.frameworkService.initialize(this.sessionContext.framework);
+    this.frameworkService.frameworkData$.pipe(first()).subscribe((frameworkDetails: any) => {
+      if (frameworkDetails && !frameworkDetails.err) {
+        this.sessionContext.frameworkData = frameworkDetails.frameworkdata[this.sessionContext.framework].categories;
+      }
+
+      console.log("program");
+      console.log(this.programDetails);
+    }, error => {
+      const errorMes = typeof _.get(error, 'error.params.errmsg') === 'string' && _.get(error, 'error.params.errmsg');
+      this.toasterService.error(errorMes || 'Fetching framework details failed');
+    });
+  }
+
   onApproveClick(nomination) {
 	this.onApprove.emit(nomination);
   }
