13b94ccb24a961fd07fb7cae3870452692040cf5
Ekta Y <ekta_y@tekditechnologies.com>
2020-03-17 17:31:16 +0530

    UI for mange user for org contributro admin

--- src/app/client/src/app/modules/contribute/components/org-user-list/org-user-list.component.ts
+++ src/app/client/src/app/modules/contribute/components/org-user-list/org-user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ResourceService } from '@sunbird/shared';
 
 @Component({
   selector: 'app-org-user-list',
@@ -9,7 +10,7 @@ export class OrgUserListComponent implements OnInit {
   data;
   options;
   showNormalModal;
-  constructor() { }
+  constructor(public resourceService: ResourceService) { }
 
   ngOnInit() {
     this.data = {
