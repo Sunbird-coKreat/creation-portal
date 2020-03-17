e57c907066b63ee08c1729c38be896a7205916a6
venkanagouda-k <shashi.kallanagoudar@gmail.com>
2020-03-18 02:11:29 +0530

    openSaberregistrySerachAPI three stages of API..

--- src/app/client/src/app/modules/shared/interfaces/userProfile.ts
+++ src/app/client/src/app/modules/shared/interfaces/userProfile.ts
@@ -64,7 +64,7 @@ export interface IUserProfile {
     tncLatestVersion?: string;
     promptTnC?: boolean;
     tncLatestVersionUrl?: string;
-    userRegData?: string;
+    userRegData?: any;
 }
 export interface IJobProfile {
     jobName: string;
