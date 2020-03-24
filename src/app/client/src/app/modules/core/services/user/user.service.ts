6079eda6e81509e071c357b45be8e8054e70c40e
Kartheek Palla <palla.kartheekreddy@gmail.com>
2020-03-24 22:13:50 +0530

    user details issue

--- src/app/client/src/app/modules/core/services/user/user.service.ts
+++ src/app/client/src/app/modules/core/services/user/user.service.ts
@@ -313,11 +313,11 @@ export class UserService {
     }
     }), tap((res3) => {
       if (res3 && res3.result.User_Org.length) {
-      this._userProfile.userRegData['User_Org'] = res3.result.User_Org;
+      this._userProfile.userRegData['User_Org'] = res3.result.User_Org[0];
       }
     }), switchMap((res4) => {
       if (res4 && res4.result.User_Org.length) {
-        const orgList = res4.result.User_Org.map((value)=> value.orgId.slice(2))
+        const orgList = res4.result.User_Org.map((value) => value.orgId.slice(2));
         option.data['request'] = {
           entityType: ['Org'],
           filters: {
@@ -331,7 +331,7 @@ export class UserService {
     })
     ).subscribe((res: any) => {
       if (res && res.result.Org.length) {
-        this._userProfile.userRegData['Org'] = res.result.Org;
+        this._userProfile.userRegData['Org'] = res.result.Org[0];
         }
         return resolve('done');
      }, (err) => {
