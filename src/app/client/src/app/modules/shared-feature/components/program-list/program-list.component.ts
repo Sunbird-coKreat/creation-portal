b69fa900f6b004f1c84ed888bf26026bbbdb0a5f
Vaishali Khandagale <vaishali_k@techjoomla.com>
2020-03-27 22:47:31 +0530

    My programs-sort program name

--- src/app/client/src/app/modules/shared-feature/components/program-list/program-list.component.ts
+++ src/app/client/src/app/modules/shared-feature/components/program-list/program-list.component.ts
@@ -239,6 +239,7 @@ export class ProgramListComponent implements OnInit {
     return this.programsService.getMyProgramsForOrg(status).subscribe((response) => {
       this.programs = _.get(response, 'result.programs');
       this.count = _.get(response, 'result.count');
+      this.sortPrograms = this.programs;
     }, error => {
       console.log(error);
       // TODO: Add error toaster
