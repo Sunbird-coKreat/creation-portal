78fe21199787ff879f43f6248b6bf69b4a4d42f7
Ekta Yadav <ekta_y@tekditechnologies.com>
2020-04-15 18:10:48 +0530

    UI fixes #418

--- src/app/client/src/app/modules/program/components/program-nominations/program-nominations.component.ts
+++ src/app/client/src/app/modules/program/components/program-nominations/program-nominations.component.ts
@@ -7,8 +7,6 @@ import { ISessionContext } from '../../../cbse-program/interfaces';
 import * as _ from 'lodash-es';
 import { tap, first } from 'rxjs/operators';
 import * as moment from 'moment';
-import {Pipe, PipeTransform} from '@angular/core';
-import { LowerCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-program-nominations',
@@ -66,7 +64,7 @@ export class ProgramNominationsComponent implements OnInit, AfterViewInit {
   this.telemetryInteractObject = {};
   this.checkActiveTab();
   this.sourcingOrgUser = this.programsService.sourcingOrgReviewers || [];
-  this.roles = [{name: 'Reviewer'}];
+  this.roles = [{name: 'REVIEWER'}];
   }
 
   ngAfterViewInit() {
