3d998b6e50c1ed4f045e3d4fc4087afd3bb73391
nilesh5995 <nilesh_s@techjoomla.com>
2020-04-15 09:20:28 +0530

    resolve the comments

--- src/app/client/src/app/modules/shared-feature/components/enroll-contributor/enroll-contributor.component.ts
+++ src/app/client/src/app/modules/shared-feature/components/enroll-contributor/enroll-contributor.component.ts
@@ -25,7 +25,6 @@ export class EnrollContributorComponent implements OnInit, AfterViewInit {
   @ViewChild('modal') modal;
   frameworkdetails;
   formIsInvalid = false;
-  isOrgNameExist = false;
   contentType = { };
   telemetryImpression: any;
   options;
