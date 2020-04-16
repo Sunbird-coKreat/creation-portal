0b9b4f1f14224b68f127086e5af0b312dcf72847
Kartheek Palla <palla.kartheekreddy@gmail.com>
2020-04-16 15:20:40 +0530

    Issue #DP-000 fix: page assemble route added

--- src/app/routes/publicRoutes.js
+++ src/app/routes/publicRoutes.js
@@ -14,6 +14,33 @@ module.exports = function (app) {
         return require('url').parse(contentProxyUrl + req.originalUrl).path
     }
 
+    app.post('/api/data/v1/page/*',
+        permissionsHelper.checkPermission(),
+        proxy(learnerURL, {
+        proxyReqOptDecorator: proxyHeaders.decorateSunbirdRequestHeaders(),
+        proxyReqPathResolver: function (req) {
+            let urlParam = req.originalUrl.replace('/api/', '')
+            let query = require('url').parse(req.url).query
+            if (query) {
+            return require('url').parse(learnerURL + urlParam + '?' + query).path
+            } else {
+            return require('url').parse(learnerURL + urlParam).path
+            }
+        },
+        userResDecorator: function (proxyRes, proxyResData,  req, res) {
+            try {
+                logger.info({msg: '/api/data/v1/page'});
+                const data = JSON.parse(proxyResData.toString('utf8'));
+                if(req.method === 'GET' && proxyRes.statusCode === 404 && (typeof data.message === 'string' && data.message.toLowerCase() === 'API not found with these values'.toLowerCase())) res.redirect('/')
+                else return proxyHeaders.handleSessionExpiry(proxyRes, proxyResData, req, res, data);
+            } catch(err) {
+                logger.error({msg:'content api user res decorator json parse error:', proxyResData})
+                    return proxyHeaders.handleSessionExpiry(proxyRes, proxyResData, req, res);
+            }
+        }
+        })
+    )
+
     app.post('/api/org/v1/search',
         permissionsHelper.checkPermission(),
         proxy(learnerURL, {
