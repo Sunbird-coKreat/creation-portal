3716a77b136817cf083e6f5d39ce0803b94da150
Venkateshwaran Selvaraj <venkateshwarans@ilimi.in>
2020-04-20 11:43:18 +0530

    Issue #DP-432 fix: Editor keeps loading - sprint6

--- src/app/client/src/app/modules/cbse-program/components/chapter-list/chapter-list.component.ts
+++ src/app/client/src/app/modules/cbse-program/components/chapter-list/chapter-list.component.ts
@@ -16,6 +16,7 @@ import { ProgramStageService } from '../../../program/services';
 import { ProgramComponentsService } from '../../../program/services/program-components/program-components.service';
 import { InitialState } from '../../interfaces';
 import { CollectionHierarchyService } from '../../services/collection-hierarchy/collection-hierarchy.service';
+import { HelperService } from '../../services/helper.service';
 
 interface IDynamicInput {
   contentUploadComponentInput?: IContentUploadComponentInput;
@@ -83,7 +84,7 @@ export class ChapterListComponent implements OnInit, OnChanges, OnDestroy, After
   constructor(public publicDataService: PublicDataService, private configService: ConfigService,
     private userService: UserService, public actionService: ActionService,
     public telemetryService: TelemetryService, private cbseService: CbseProgramService,
-    public toasterService: ToasterService, public router: Router,
+    public toasterService: ToasterService, public router: Router, public helperService: HelperService,
     public programStageService: ProgramStageService, public programComponentsService: ProgramComponentsService,
     public activeRoute: ActivatedRoute, private ref: ChangeDetectorRef,
     private collectionHierarchyService: CollectionHierarchyService, private resourceService: ResourceService,
@@ -656,8 +657,25 @@ export class ChapterListComponent implements OnInit, OnChanges, OnDestroy, After
        .subscribe(() => {
          this.showConfirmationModal = false;
          this.updateAccordianView(this.unitIdentifier);
-        this.toasterService.success(this.resourceService.messages.smsg.m0064);
+         this.resetContentId();
+         this.toasterService.success(this.resourceService.messages.smsg.m0064);
        });
+  }
+
+  deleteContent() {
+    this.helperService.retireContent(this.contentId)
+      .subscribe(
+        (response) => {
+          if (response && response.result && response.result.node_id) {
+            this.removeResourceFromHierarchy();
+          } else {
+            this.toasterService.error(this.resourceService.messages.fmsg.m00103);
+          }
+        },
+        (error) => {
+          this.toasterService.error(this.resourceService.messages.fmsg.m00103);
+        }
+      );
   }
 
   handleBack() {
