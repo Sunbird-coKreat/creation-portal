68edc4a983c965e8d7283380a22020cb4c0468ca
Vaishali Khandagale <vaishali_k@techjoomla.com>
2020-05-14 18:01:02 +0530

    Fix: remove sample contents from nominations view

--- src/app/client/src/app/modules/core/services/registry/registry.service.ts
+++ src/app/client/src/app/modules/core/services/registry/registry.service.ts
@@ -33,23 +33,26 @@ export class RegistryService extends DataService {
   public getcontributingOrgUsersDetails() {
     const userRegData = _.get(this.userService, 'userProfile.userRegData');
     const orgId = userRegData.User_Org.orgId;
+    const storedOrglist = this.cacheService.get('orgUsersDetails')
     return new Promise((resolve, reject) => {
       if (this.checkIfUserBelongsToOrg()) {
-          return this.getContributionOrgUsers(orgId).subscribe(
-            (res1) => {
-              if (res1.result.User_Org.length && res1.result.User_Org.length !== this.cacheService.get('orgUsersDetails').length) {
+        return this.getContributionOrgUsers(orgId).subscribe(
+          (res1) => {
+            const tempMapping = [];
+
+            if (res1.result.User_Org.length) {
+              const userList = _.uniq(_.map(
+                _.filter(res1.result.User_Org, obj => { if (obj.userId !== userRegData.User.osid) { return obj } }),
+                (mapObj) => {
+                  tempMapping.push(mapObj);
+                  return mapObj.userId.slice(2);
+                }));
+
+              if (userList && storedOrglist && userList.length === storedOrglist.length) {
                 return resolve(this.cacheService.get('orgUsersDetails'));
               } else {
                 let orgUsersDetails = {};
-                const tempMapping = [];
                 const tempUser = [];
-                if (res1.result.User_Org.length) {
-                  const userList = _.uniq(_.map(
-                  _.filter(res1.result.User_Org, obj => { if(obj.userId !== userRegData.User.osid) {return obj }}),
-                    (mapObj) => {
-                      tempMapping.push(mapObj);
-                    return mapObj.userId.slice(2);
-                  }));
 
                 return this.getUserdetailsByOsIds(userList).pipe(
                   switchMap((res2: any) => {
@@ -64,7 +67,7 @@ export class RegistryService extends DataService {
                         data: {
                           'request': {
                             'filters': {
-                                'identifier': _.compact(userList)
+                              'identifier': _.compact(userList)
                             }
                           }
                         }
@@ -76,22 +79,25 @@ export class RegistryService extends DataService {
                   })).subscribe((res) => {
                     orgUsersDetails = _.get(res, 'result.response.content');
                     const userList = _.map(orgUsersDetails, (obj) => {
-                      const tempUserObj = _.find(tempUser, {'userId': obj.identifier});
+                      const tempUserObj = _.find(tempUser, { 'userId': obj.identifier });
                       obj.name = `${obj.firstName} ${obj.lastName || ''}`;
-                      obj.User = _.find(tempUser, {'userId': obj.identifier});
-                      obj.User_Org = _.find(tempMapping, {'userId': _.get(tempUserObj, 'osid') });
+                      obj.User = _.find(tempUser, { 'userId': obj.identifier });
+                      obj.User_Org = _.find(tempMapping, { 'userId': _.get(tempUserObj, 'osid') });
                       obj.selectedRole = _.first(obj.User_Org.roles);
                       return obj;
                     });
                     this.cacheService.set('orgUsersDetails', _.get(res, 'result.response.content'));
                     return resolve(this.cacheService.get('orgUsersDetails'));
                   }, (err) => { console.log(err); return reject([]); });
-                } else { return resolve([]); }
               }
-            }, (error) => { console.log(error); return reject([]);
+            } else {
+              return resolve([]);
+            }
+          }, (error) => {
+            console.log(error); return reject([]);
           });
       } else {
-        return reject([]);
+        return resolve([]);
       }
     });
   }
@@ -111,7 +117,7 @@ export class RegistryService extends DataService {
         request: {
           entityType: ['User'],
           filters: {
-            osid: {or: userList}
+            osid: { or: userList }
           }
         }
       }
@@ -155,9 +161,9 @@ export class RegistryService extends DataService {
           'msgid': ''
         },
         'request': {
-           'User': {
-             'osid': userId
-           }
+          'User': {
+            'osid': userId
+          }
         }
       }
     };
@@ -183,53 +189,53 @@ export class RegistryService extends DataService {
     option.data['request'] = {
       entityType: ['User'],
       filters: {
-        userId: {eq: userId}
+        userId: { eq: userId }
       }
-   };
-  return new Promise((resolve, reject) => {
-    this.contentService.post(option).pipe(tap((res1) => {
-      if (res1.result.User.length) {
-        userProfile['user'] = res1.result.User[0];
-      }
-    }), switchMap((res2) => {
-      if (res2.result.User.length) {
-      option.data['request'] = {
-        entityType: ['User_Org'],
-        filters: {
-        userId: {eq: res2.result.User[0].osid}
+    };
+    return new Promise((resolve, reject) => {
+      this.contentService.post(option).pipe(tap((res1) => {
+        if (res1.result.User.length) {
+          userProfile['user'] = res1.result.User[0];
         }
-     };
-     return this.contentService.post(option);
-    } else {
-      return of(null);
-    }
-    }), tap((res3) => {
-      if (res3 && res3.result.User_Org.length) {
-        userProfile['user_org'] = res3.result.User_Org[0];
-      }
-    }), switchMap((res4) => {
-      if (res4 && res4.result.User_Org.length) {
-        const orgList = res4.result.User_Org.map((value) => value.orgId.slice(2));
-        option.data['request'] = {
-          entityType: ['Org'],
-          filters: {
-            osid: {or: orgList}
-          }
-      };
-       return this.contentService.post(option);
-      } else {
-        return of(null);
-      }
-    })
-    ).subscribe((res: any) => {
-      if (res && res.result.Org.length) {
-        userProfile['org'] = res.result.Org[0];
+      }), switchMap((res2) => {
+        if (res2.result.User.length) {
+          option.data['request'] = {
+            entityType: ['User_Org'],
+            filters: {
+              userId: { eq: res2.result.User[0].osid }
+            }
+          };
+          return this.contentService.post(option);
+        } else {
+          return of(null);
+        }
+      }), tap((res3) => {
+        if (res3 && res3.result.User_Org.length) {
+          userProfile['user_org'] = res3.result.User_Org[0];
+        }
+      }), switchMap((res4) => {
+        if (res4 && res4.result.User_Org.length) {
+          const orgList = res4.result.User_Org.map((value) => value.orgId.slice(2));
+          option.data['request'] = {
+            entityType: ['Org'],
+            filters: {
+              osid: { or: orgList }
+            }
+          };
+          return this.contentService.post(option);
+        } else {
+          return of(null);
+        }
+      })
+      ).subscribe((res: any) => {
+        if (res && res.result.Org.length) {
+          userProfile['org'] = res.result.Org[0];
         }
         userProfile['error'] = false;
         return resolve(userProfile);
-     }, (err) => {
-      return reject(userProfile);
-     });
+      }, (err) => {
+        return reject(userProfile);
+      });
     });
   }
 }
