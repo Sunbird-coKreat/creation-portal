2e5831fe44d6b3598759e8019a8c08d2398f6a3c
JagadishPujari <jagadish.pujari@tarento.com>
2020-06-22 22:44:51 +0530

    Issue #DP-457 feat: Resolved review comments

--- src/app/tenant/ntp/js/custom.js
+++ src/app/tenant/ntp/js/custom.js
@@ -60,36 +60,49 @@ function makeCallToGetProjects(url, callback){
 }
 
 function getProjectsTemplates(data){
-  addOtherBoardTemplate(data);
-  addAllStateBoardTemplaet(data);
-  var a = window.location.hostname;
-  $(".cb").attr("href",getEnvironment(a));
-}
-function addOtherBoardTemplate(data){
-  var otherBoards = data.filter(e => (e.org_name === 'CBSE') || e.org_name === 'NCERT');
   var otherBoardCards = '';
-  for(let i=0; i<otherBoards.length; i++){
-    otherBoardCards = otherBoardCards + '<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xs-6"> <div class="program-cards"> <div class="topSection"> <label class="text-center hidden-lg-down">' + otherBoards[i].orgName + '</label> <div class="my-20">';
-    if(otherBoardCards[i].imgUrl){
-      otherBoardCards = otherBoardCards + '<img src="' + otherBoards[i].imgUrl + '" class="logo-img" alt="' + otherBoards[i].orgName + ' logo" />'
+  var stateBoardCards = '';
+  for(let i=0; i<data.length; i++){
+    //Other state boards
+    if(data[i].orgName == 'NCERT' || data[i].orgName == 'CBSE'){
+      otherBoardCards = otherBoardCards + '<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xs-6"> <div class="program-cards"> <div class="topSection"> <label class="text-center hidden-lg-down">' + data[i].orgName + '</label> <div class="my-20">';
+    checkImageExists(data[i].imgUrl, function(imgPath){
+      if(imgPath){
+        otherBoardCards = otherBoardCards + '<img src="' + imgPath + '" class="logo-img" alt="' + data[i].orgName + ' logo" />'
+      }
+    });
+    otherBoardCards = otherBoardCards + '</div> </div> <div class="bottomSection"> <label><span class="fs-2">'+ data[i].program_count + '</span> Projects</label> <a href="#" class="hidden-lg-down a-link cb">Contribute</a> </div> </div> </div>';
+    }else{
+      // state boards
+      checkImageExists(data[i].imgUrl, function(imgPath){
+        stateBoardCards = stateBoardCards + '<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xs-6"> <div class="program-cards"> <div class="topSection"> <label class="text-center hidden-lg-down">' + data[i].orgName + '</label> <div class="my-20">';
+        if(imgPath){
+          // console.log('hey');
+          stateBoardCards = stateBoardCards + '<img src="' + imgPath + '" class="logo-img" alt="' + data[i].orgName + ' logo"/>';
+        }
+        stateBoardCards = stateBoardCards + '</div> </div> <div class="bottomSection"> <label><span class="fs-2">' + data[i].program_count + '</span> Projects</label> <a href="#" class="hidden-lg-down a-link cb">Contribute</a> </div> </div> </div>';
+      });
     }
-    otherBoardCards = otherBoardCards + '</div> </div> <div class="bottomSection"> <label><span class="fs-2">'+ otherBoards[i].pgm_count + '</span> Projects</label> <a href="#" class="hidden-lg-down a-link cb">Contribute</a> </div> </div> </div>';
   }
-  $('.flex-jc-center').append(otherBoardCards);
+  setTimeout(function(){ 
+    $('.flex-jc-center').append(otherBoardCards);
+    $('.state-board-projects').append(stateBoardCards); 
+    var a = window.location.hostname;
+    $(".cb").attr("href",getEnvironment(a));
+  }, 500);
 }
-function addAllStateBoardTemplaet(data) {
-  var allStateBoards = data.filter(function(el){return el.org_name !== "CBSE" && el.org_name !== "NCERT";});
-  var stateCards = '';
-  for(let j=0; j<allStateBoards.length; j++){
-    stateCards = stateCards + '<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xs-6"> <div class="program-cards"> <div class="topSection"> <label class="text-center hidden-lg-down">' + allStateBoards[j].orgName + '</label> <div class="my-20">';
-    if(allStateBoards[j].imgUrl){
-      stateCards = stateCards + '<img src="' + allStateBoards[j].imgUrl + '" class="logo-img" alt="' + allStateBoards[j].orgName + ' logo"/>';
-    }
-    stateCards = stateCards + '</div> </div> <div class="bottomSection"> <label><span class="fs-2">' + allStateBoards[j].program_count + '</span> Projects</label> <a href="#" class="hidden-lg-down a-link cb">Contribute</a> </div> </div> </div>';
+//Get image path
+function checkImageExists(slug, callBack) {
+  var imageUrl = window.location.origin + '/' + slug + '/logo.png';
+  var imageData = new Image();
+  imageData.onload = function() {
+    callBack(imageUrl);
+  };
+  imageData.onerror = function() {
+    callBack(false);
+  };
+  imageData.src = imageUrl;
   }
-  $('.state-board-projects').append(stateCards);
-}
-
   var $videoSrc;  
   $('.video-btn').click(function() {
       $videoSrc = $(this).data( "src" );
