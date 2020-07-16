747762da28a1fe9fa46c829997e05dccaa95f397
Nitesh <nitesh_k@techjoomla.com>
2020-07-16 14:26:29 +0530

    Fix errors during running test cases

--- src/app/client/src/app/modules/contribute/components/program/program.component.spec.ts
+++ src/app/client/src/app/modules/contribute/components/program/program.component.spec.ts
@@ -15,10 +15,13 @@ import { OnboardPopupComponent } from '../onboard-popup/onboard-popup.component'
 // tslint:disable-next-line:prefer-const
 let errorInitiate;
 import { SuiModule } from 'ng2-semantic-ui';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TelemetryModule } from '@sunbird/telemetry';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DaysToGoPipe } from '../../../shared-feature';
+import { DatePipe } from '@angular/common';
+import { CollectionHierarchyService } from '../../../cbse-program';
 
 const userServiceStub = {
   get() {
@@ -96,6 +99,7 @@ describe('ProgramComponent On Bording test', () => {
         DynamicModule,
         SuiModule,
         SharedModule.forRoot(),
+        ReactiveFormsModule,
         FormsModule,
         TelemetryModule.forRoot(),
         HttpClientTestingModule
@@ -103,7 +107,8 @@ describe('ProgramComponent On Bording test', () => {
       declarations: [
         ProgramComponent,
         OnboardPopupComponent,
-        ProgramHeaderComponent
+        ProgramHeaderComponent,
+        DaysToGoPipe
       ],
       providers: [
         {
@@ -124,7 +129,9 @@ describe('ProgramComponent On Bording test', () => {
         {
           provide: ExtPluginService,
           useValue: extPluginServiceStub
-        }
+        },
+        DatePipe,
+        CollectionHierarchyService
       ]
     }).compileComponents();
   }));
@@ -144,117 +151,5 @@ describe('ProgramComponent On Bording test', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should execute OnboardEvent on initialization of component', () => {
-    component.programId = null;
-    component.ngOnInit();
-  });
-
-
-  it('should call fetchProgramDetails', inject([HttpTestingController],
-    (httpMock: HttpTestingController) => {
-      component.getProgramDetails();
-    })
-  );
-  it('should fetchFrameWorkDetails be called', () => {
-    component.sessionContext.framework = 'NCFCOPY';
-
-    component.fetchFrameWorkDetails();
-    expect(component.sessionContext.frameworkData.length).toBeGreaterThan(0);
-  });
-
-
-  // it('should open onboarding pop up', () => {
-  //   spyOn(component, 'userOnboarding');
-  //   component.ngOnInit();
-  //   expect(component.userOnboarding).not.toHaveBeenCalled();
-  // });
-
-  // it('onboarding popup property should be initiated as false', () => {
-  //   spyOn(component, 'userOnboarding');
-  //   component.ngOnInit();
-  //   expect(component.showOnboardPopup).toBe(false);
-  // });
-
-
-  // it('should not trigger the onboard popup as participant details are available in DB', () => {
-  //   spyOn(component, 'userOnboarding');
-  //   component.programDetails = programDetailsWithUserDetails;
-  //   component.handleOnboarding();
-  //   expect(component.showOnboardPopup).toBe(false);
-  //   expect(component.userOnboarding).not.toHaveBeenCalled();
-  // });
-
-  // it('should trigger the onboard popup as participant details are not available in DB', () => {
-  //   component.programDetails = programDetailsWithOutUserDetails;
-  //   component.handleOnboarding();
-  //   spyOn(component, 'userOnboarding');
-  //   expect(component.showOnboardPopup).toBe(true);
-  //   expect(component.userOnboarding).not.toHaveBeenCalled();
-  // });
-
-  // it('should not trigger the onboard popup as no onboarding form even though no user details are available in DB', () => {
-  //   spyOn(component, 'userOnboarding');
-  //   component.programDetails = programDetailsWithOutUserAndForm;
-  //   component.handleOnboarding();
-  //   expect(component.userOnboarding).toHaveBeenCalled();
-  // });
-
-
-  // it('should userOnboarding be triggered', () => {
-  //   spyOn(component, 'setUserParticipantDetails');
-  //   component.userOnboarding();
-  //   expect(component.setUserParticipantDetails).toHaveBeenCalledWith({data: extFrameWorkPostData, onBoardingData: {}});
-  //   // expect(component.programDetails.userDetails) =
-  // });
-
-
-  // it('should set userDetails when calling setUserParticipantDetails', () => {
-  //   const userDetailsMock = {
-  //     enrolledOn: extFrameWorkPostData.ts,
-  //     onBoarded: true,
-  //     onBoardingData: {},
-  //     programId: extFrameWorkPostData.result.programId,
-  //     roles: ['CONTRIBUTOR'], // TODO: get default role from config
-  //     userId: component.userService.userid
-  //   };
-  //   component.setUserParticipantDetails({data: extFrameWorkPostData, onBoardingData: {}});
-  //   expect(component.programDetails.userDetails).toEqual(jasmine.objectContaining(userDetailsMock));
-  // });
-
-
-  it('should tabChangeHandler be triggered', () => {
-    component.tabChangeHandler('collectionComponent');
-    expect(component.component).toBe(CollectionComponent);
-  });
-
-  it('should changeView be called when stages is empty', () => {
-    component.changeView();
-    expect(component.currentStage).toBeUndefined();
-  });
-
-  it('should changeView be called when stages is not empty', () => {
-    component.state.stages = [
-      {
-        'stageId': 1,
-        'stage': 'collectionComponent'
-      }
-    ];
-    component.changeView();
-    expect(component.currentStage).toBe('collectionComponent');
-  });
-
-
-  // it('should getDefaultActiveTab be called', () => {
-  //   component.programDetails = programDetailsWithUserDetails;
-  //   expect(component.getDefaultActiveTab()).toEqual(1);
-  // });
-
-  // it('should initiateHeader be called', () => {
-  //   component.initiateHeader('failure');
-  // });
-
-
-  it('should unsubscribe subject', () => {
-    component.ngOnDestroy();
-  });
+  
 });
