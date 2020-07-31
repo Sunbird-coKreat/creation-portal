87038a187026a1b3e6d3f391f95fdc24ce15ceae
Amol Ghatol <amol_g@techjoomla.com>
2020-07-31 11:55:32 +0530

    Issue DP-844 feat: Guideline doc missing in re-edit draft

--- src/app/client/src/app/modules/core/services/programs/programs.service.ts
+++ src/app/client/src/app/modules/core/services/programs/programs.service.ts
@@ -38,8 +38,7 @@ export class ProgramsService extends DataService implements CanActivate {
   public config: ConfigService;
   baseUrl: string;
   public http: HttpClient;
-  // private API_URL = this.publicDataService.post; // TODO: remove API_URL once service is deployed
-  private API_URL = this.post; // TODO: remove API_URL once service is deployed
+  private API_URL = this.publicDataService.post; // TODO: remove API_URL once service is deployed
 
   private _contentTypes: any[];
   private _sourcingOrgReviewers: Array<any>;
@@ -54,8 +53,7 @@ export class ProgramsService extends DataService implements CanActivate {
     public cacheService: CacheService) {
       super(http);
       this.config = config;
-      // this.baseUrl = this.config.urlConFig.URLS.CONTENT_PREFIX;
-      this.baseUrl = 'http://localhost:6000/';
+      this.baseUrl = this.config.urlConFig.URLS.CONTENT_PREFIX;
     }
 
   /**
