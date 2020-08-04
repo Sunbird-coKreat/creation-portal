c15f5079aef7f3d5e5abbecda79728a48ebb6b6d
Santosh M <santoshm@santoshs-MacBook-Pro.local>
2020-08-04 11:11:16 +0530

    Issue #DP-775 feat: MVC content - Remove functionality from ChapterList

--- src/app/client/src/app/modules/cbse-program/components/mvc-library/mvc-library.component.ts
+++ src/app/client/src/app/modules/cbse-program/components/mvc-library/mvc-library.component.ts
@@ -224,6 +224,7 @@ export class MvcLibraryComponent implements OnInit, AfterViewInit {
       });
   }
 
+
   filterContentList(selectedContentId?) {
     if (_.isEmpty(this.contentList)) { return; }
     _.forEach(this.contentList, (value, key) => {
