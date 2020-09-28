47012ea47a06bea73da5f0fbb60b171046d8737d
“Prashant <prashant_s@techjoomla.com>
2020-09-28 13:19:05 +0530

    Issue #DP-369 Fix: User is not navigated to top of the page on click of next button in create program form

--- src/app/client/src/app/app.routing.ts
+++ src/app/client/src/app/app.routing.ts
@@ -47,7 +47,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, {scrollPositionRestoration: 'top'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
