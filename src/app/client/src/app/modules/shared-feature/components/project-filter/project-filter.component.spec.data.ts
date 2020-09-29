f277f57b60a0498840a7b68a2d438e8e4a8a10e3
Santosh M <santoshm@santoshs-MacBook-Pro.local>
2020-09-29 11:46:28 +0530

    fix test cases

---
+++ src/app/client/src/app/modules/shared-feature/components/project-filter/project-filter.component.spec.data.ts
@@ -0,0 +1,238 @@
+export const programData = {
+    programs: [
+        {
+            "id": 13211,
+            "program_id": "dc9dd2c0-a759-11ea-a298-7d68c74f37f8",
+            "user_id": "e73b4786-a6b3-4fe7-b02b-81560ca87811",
+            "organisation_id": null,
+            "status": "Pending",
+            "content_types": [
+                "TeachingMethod"
+            ],
+            "collection_ids": [
+                "do_1130365972991344641450"
+            ],
+            "feedback": null,
+            "rolemapping": null,
+            "createdby": "6582c32c-0758-4341-abfd-b3aab6ebe7e1",
+            "updatedby": null,
+            "createdon": "2020-09-09T13:23:51.538Z",
+            "updatedon": null,
+            "program": {
+                "program_id": "dc9dd2c0-a759-11ea-a298-7d68c74f37f8",
+                "name": "TEST DATA PROGRAM-DO NOT DELETE",
+                "type": "public",
+                "collection_ids": [
+                    "do_11303435783214694411197"
+                ],
+                "content_types": [
+                    "TeachingMethod",
+                    "PedagogyFlow",
+                    "FocusSpot",
+                    "LearningOutcomeDefinition",
+                    "MarkingSchemeRubric",
+                    "PracticeQuestionSet",
+                    "SelfAssess",
+                    "LearningActivity",
+                    "ExplanationReadingMaterial"
+                ],
+                "startdate": "2020-06-05T18:24:58.085Z",
+                "enddate": "2021-11-27T18:30:00.000Z",
+                "nomination_enddate": "2021-01-09T18:30:00.000Z",
+                "shortlisting_enddate": "2021-05-29T18:30:00.000Z",
+                "content_submission_enddate": "2021-08-21T18:30:00.000Z",
+                "image": null,
+                "status": "Live",
+                "slug": "sunbird",
+                "rolemapping": {
+                    "REVIEWER": [
+                        "e53cb0b9-922d-4d9e-8b43-5f4dc23bc085"
+                    ]
+                },
+                "createdby": "ad1d9730-d79d-4724-b378-0279c55815fd",
+                "updatedby": null,
+                "createdon": "2020-06-05T18:24:58.085Z",
+                "updatedon": "2020-08-07T09:19:43.488Z",
+                "rootorg_id": "01303420140154880043",
+                "sourcing_org_name": "Green_wood",
+                "channel": "DIKSHA",
+                "template_id": "template1",
+                "guidelines_url": "",
+                "subject": "[\"Mathematics\"]",
+                "defaultContributeOrgReview": null,
+                "framework": "ekstep_ncert_k-12",
+                "board": "CBSE",
+                "gradeLevel": "[\"Class 1\"]",
+                "medium": "[\"English\"]"
+            }
+        },
+        {
+            "id": 13185,
+            "program_id": "1a0d7dd0-ee69-11ea-a157-278232e1d499",
+            "user_id": "e73b4786-a6b3-4fe7-b02b-81560ca87811",
+            "organisation_id": null,
+            "status": "Pending",
+            "content_types": [
+                "ExplanationVideo"
+            ],
+            "collection_ids": [
+                "do_11310060338919014411712"
+            ],
+            "feedback": null,
+            "rolemapping": null,
+            "createdby": "6582c32c-0758-4341-abfd-b3aab6ebe7e1",
+            "updatedby": null,
+            "createdon": "2020-09-09T12:48:50.236Z",
+            "updatedon": null,
+            "program": {
+                "program_id": "1a0d7dd0-ee69-11ea-a157-278232e1d499",
+                "name": "GRADE 7 Math",
+                "type": "public",
+                "collection_ids": [
+                    "do_1130956340952104961166"
+                ],
+                "content_types": [
+                    "ExplanationVideo",
+                    "PreviousBoardExamPapers"
+                ],
+                "startdate": "2020-06-01T00:00:00.000Z",
+                "enddate": "2020-10-16T00:00:00.000Z",
+                "nomination_enddate": "2020-10-05T00:00:00.000Z",
+                "shortlisting_enddate": "2020-10-10T00:00:00.000Z",
+                "content_submission_enddate": "2020-10-15T00:00:00.000Z",
+                "image": null,
+                "status": "Live",
+                "slug": "sunbird",
+                "rolemapping": {
+                    "REVIEWER": [
+                        "a6bc2082-0c08-4dce-a704-b04d5a0e481a"
+                    ]
+                },
+                "createdby": "5a587cc1-e018-4859-a0a8-e842650b9d64",
+                "updatedby": null,
+                "createdon": "2020-09-04T04:45:48.605Z",
+                "updatedon": "2020-09-09T10:31:10.880Z",
+                "rootorg_id": "01309282781705830427",
+                "sourcing_org_name": "NIT",
+                "channel": "DIKSHA",
+                "template_id": "template1",
+                "guidelines_url": null,
+                "subject": "[\"Mathematics\"]",
+                "defaultContributeOrgReview": "true",
+                "framework": "ekstep_ncert_k-12",
+                "board": "CBSE",
+                "gradeLevel": "[\"Class 7\"]",
+                "medium": "[\"English\"]"
+            }
+        },
+        {
+            "id": 13471,
+            "program_id": "de0f50d0-0171-11eb-90f6-35d24aa9a959",
+            "user_id": "e73b4786-a6b3-4fe7-b02b-81560ca87811",
+            "organisation_id": null,
+            "status": "Pending",
+            "content_types": [
+                "TeachingMethod",
+                "PedagogyFlow",
+                "FocusSpot",
+                "LearningOutcomeDefinition",
+                "PracticeQuestionSet",
+                "CuriosityQuestionSet",
+                "MarkingSchemeRubric"
+            ],
+            "collection_ids": [
+                "do_1131177466774405121681"
+            ],
+            "feedback": null,
+            "rolemapping": null,
+            "createdby": "6582c32c-0758-4341-abfd-b3aab6ebe7e1",
+            "updatedby": null,
+            "createdon": "2020-09-29T04:38:37.723Z",
+            "updatedon": null,
+            "program": {
+                "program_id": "de0f50d0-0171-11eb-90f6-35d24aa9a959",
+                "name": "OrganisationReports",
+                "type": "public",
+                "collection_ids": [
+                    "do_11311774533553356814"
+                ],
+                "content_types": [
+                    "TeachingMethod",
+                    "PedagogyFlow",
+                    "FocusSpot",
+                    "LearningOutcomeDefinition",
+                    "PracticeQuestionSet",
+                    "CuriosityQuestionSet",
+                    "MarkingSchemeRubric",
+                    "ExplanationResource",
+                    "ExperientialResource",
+                    "ConceptMap",
+                    "SelfAssess",
+                    "ExplanationVideo",
+                    "ClassroomTeachingVideo",
+                    "ExplanationReadingMaterial",
+                    "LearningActivity",
+                    "PreviousBoardExamPapers",
+                    "LessonPlanResource"
+                ],
+                "startdate": "2020-09-28T10:03:55.394Z",
+                "enddate": "2021-01-09T18:30:00.000Z",
+                "nomination_enddate": "2020-10-10T18:30:00.000Z",
+                "shortlisting_enddate": "2020-11-07T18:30:00.000Z",
+                "content_submission_enddate": "2020-12-05T18:30:00.000Z",
+                "image": null,
+                "status": "Live",
+                "slug": "sunbird",
+                "rolemapping": null,
+                "createdby": "5a587cc1-e018-4859-a0a8-e842650b9d64",
+                "updatedby": null,
+                "createdon": "2020-09-28T10:03:55.394Z",
+                "updatedon": "2020-09-28T10:03:56.360Z",
+                "rootorg_id": "01309282781705830427",
+                "sourcing_org_name": "NIT",
+                "channel": "DIKSHA",
+                "template_id": "template1",
+                "guidelines_url": null,
+                "subject": "[\"Mathematics\"]",
+                "defaultContributeOrgReview": "true",
+                "framework": "NCF",
+                "board": null,
+                "gradeLevel": "[\"Class 1\"]",
+                "medium": "[\"English\"]"
+            }
+        },
+    ],
+    applyFilters: {
+    filterGradeLevel : [
+        'Class 1',
+        'Class 7',
+    ],
+    filterMedium : [
+        'English',
+        'Hindi',
+    ],
+    filterSubject : [
+        'Mathematics',
+    ],
+    filterContentTypes : [
+        "TeachingMethod",
+        "PedagogyFlow",
+        "FocusSpot",
+        "LearningOutcomeDefinition"
+        ,"PracticeQuestionSet",
+        "CuriosityQuestionSet",
+        "MarkingSchemeRubric",
+        "ExplanationResource",
+        "ConceptMap",
+        "ExperientialResource",
+        "SelfAssess",
+        "ExplanationVideo",
+        "ClassroomTeachingVideo",
+        "PreviousBoardExamPapers",
+        "ExplanationReadingMaterial",
+        "LearningActivity",
+        "LessonPlanResource"
+    ]
+}
+}
+
