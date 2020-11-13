3072d70014d30805f406e12f90c34ae0020e00ec
Kartheek Palla <palla.kartheekreddy@gmail.com>
2020-11-13 12:14:37 +0530

    Issue #DP-000 fix: object category definition scripts updated

--- definition-scripts/Explanation_content.sh
+++ definition-scripts/Explanation_content.sh
@@ -59,90 +59,17 @@ curl -L -X POST '{{host}}/object/category/definition/v4/create' \
               "description": "Name",
               "index": 1,
               "label": "Name",
-              "required": false,
+              "required": true,
               "name": "Name",
               "inputType": "text",
               "placeholder": "Name"
-            },
-            {
-              "code": "board",
-              "depends": [
-                "medium",
-                "gradeLevel",
-                "subject"
-              ],
-              "editable": true,
-              "displayProperty": "Editable",
-              "dataType": "text",
-              "renderingHints": {
-                "semanticColumnWidth": "six"
-              },
-              "description": "Education Board (Like MP Board, NCERT, etc)",
-              "index": 2,
-              "label": "Board",
-              "required": false,
-              "name": "Board",
-              "inputType": "select",
-              "placeholder": "Board"
-            },
-            {
-              "code": "medium",
-              "depends": [
-                "gradeLevel",
-                "subject"
-              ],
-              "editable": true,
-              "dataType": "list",
-              "renderingHints": {
-                "semanticColumnWidth": "six"
-              },
-              "description": "Medium of instruction",
-              "index": 3,
-              "label": "Medium",
-              "required": false,
-              "name": "Medium",
-              "inputType": "multiSelect",
-              "placeholder": "Medium"
-            },
-            {
-              "code": "gradeLevel",
-              "depends": [
-                "subject"
-              ],
-              "editable": true,
-              "dataType": "list",
-              "renderingHints": {
-                "semanticColumnWidth": "six"
-              },
-              "description": "Grade",
-              "index": 4,
-              "label": "Grade",
-              "required": false,
-              "name": "Grade",
-              "inputType": "multiSelect",
-              "placeholder": "Grade"
-            },
-            {
-              "code": "subject",
-              "editable": true,
-              "dataType": "list",
-              "renderingHints": {
-                "semanticColumnWidth": "six"
-              },
-              "name": "Subject",
-              "description": "Subject of the Content to use to teach",
-              "index": 5,
-              "inputType": "multiSelect",
-              "label": "Subject",
-              "placeholder": "Grade",
-              "required": false
             },
             {
               "code": "learningOutcome",
               "dataType": "list",
               "description": "",
               "editable": true,
-              "index": 6,
+              "index": 2,
               "inputType": "select",
               "label": "Learning Outcome :",
               "name": "Learning Outcome :",
@@ -158,7 +85,7 @@ curl -L -X POST '{{host}}/object/category/definition/v4/create' \
               "dataType": "list",
               "description": "Attributions",
               "editable": true,
-              "index": 7,
+              "index": 3,
               "inputType": "text",
               "label": "Attributions",
               "name": "attribution",
@@ -172,7 +99,7 @@ curl -L -X POST '{{host}}/object/category/definition/v4/create' \
               "dataType": "text",
               "description": "Author",
               "editable": true,
-              "index": 8,
+              "index": 4,
               "inputType": "text",
               "label": "Author",
               "name": "Author",
@@ -186,14 +113,49 @@ curl -L -X POST '{{host}}/object/category/definition/v4/create' \
               "dataType": "text",
               "description": "Copyright",
               "editable": true,
-              "index": 9,
+              "index": 5,
               "inputType": "text",
-              "label": "Copyright",
+              "label": "Copyright and Year",
               "name": "Copyright",
               "placeholder": "Copyright",
               "tooltip": "If you are an individual, creating original content, you are the copyright holder. If you are creating this course content on behalf of an organisation, the organisation may be the copyright holder. ",
               "renderingHints": {},
-              "required": false
+              "required": true
+            },
+            {
+              "code": "license",
+              "visible": true,
+              "editable": true,
+              "displayProperty": "Editable",
+              "dataType": "text",
+              "renderingHints": {
+                "semanticColumnWidth": "six"
+              },
+              "description": "Subject of the Content to use to teach",
+              "index": 6,
+              "label": "License:",
+              "required": true,
+              "name": "license",
+              "inputType": "select",
+              "placeholder": "license",
+              "tooltip": "Choose the more appropriate Creative commons license for this Content. "
+            },
+            {
+              "code": "additionalCategories",
+              "visible": true,
+              "editable": true,
+              "displayProperty": "Editable",
+              "dataType": "list",
+              "renderingHints": {
+                "semanticColumnWidth": "six"
+              },
+              "description": "Subject of the Content to use to teach",
+              "index": 7,
+              "label": "Content Additional Categories",
+              "required": false,
+              "name": "additionalCategories",
+              "inputType": "multiSelect",
+              "placeholder": "Content Additional Categories"
             }
           ]
         }
