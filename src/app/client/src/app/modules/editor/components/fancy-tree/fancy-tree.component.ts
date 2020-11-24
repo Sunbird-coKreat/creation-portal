42fbb9c24b8bd5f7a90dc8e318a4fce835312b85
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2020-11-24 16:56:33 +0530

    Issue #DP-1039 fix: Add question functionality

--- src/app/client/src/app/modules/editor/components/fancy-tree/fancy-tree.component.ts
+++ src/app/client/src/app/modules/editor/components/fancy-tree/fancy-tree.component.ts
@@ -4,7 +4,7 @@ import { IFancytreeOptions } from '@sunbird/shared';
 import * as _ from 'lodash-es';
 import { ActivatedRoute } from '@angular/router';
 import { TreeService } from '../../services';
-
+import {editorConfig} from '../../editor.config';
 @Component({
   selector: 'app-fancy-tree',
   templateUrl: './fancy-tree.component.html'
@@ -13,415 +13,8 @@ export class FancyTreeComponent implements AfterViewInit {
   @ViewChild('fancyTree') public tree: ElementRef;
   @Input() public nodes: any;
   @Input() public options: any;
-  @Input() public rootNode;
   @Output() public treeEventEmitter: EventEmitter<any> = new EventEmitter();
-  config: any = {
-    'baseURL': '',
-    'corePluginsPackaged': true,
-    'pluginRepo': '/plugins',
-    'dispatcher': 'console',
-    'apislug': '/action',
-    'nodeDisplayCriteria': {
-      'contentType': ['TextBookUnit']
-    },
-    'editorType': 'Collection',
-    'collectionEditorPlugins': [{
-      'id': 'org.ekstep.sunbirdcommonheader',
-      'ver': '1.9',
-      'type': 'plugin'
-    },
-    {
-      'id': 'org.ekstep.sunbirdmetadata',
-      'ver': '1.1',
-      'type': 'plugin'
-    },
-    {
-      'id': 'org.ekstep.metadata',
-      'ver': '1.5',
-      'type': 'plugin'
-    },
-    {
-      'id': 'org.ekstep.lessonbrowser',
-      'ver': '1.5',
-      'type': 'plugin'
-    }
-    ],
-    keywordsLimit: 500,
-    editorConfig: {
-      'rules': {
-        'levels': 7,
-        'objectTypes': [
-          {
-            'type': 'TextBook',
-            'label': 'Textbook',
-            'isRoot': true,
-            'editable': true,
-            'childrenTypes': [
-              'TextBookUnit',
-              'Collection',
-              'Resource'
-            ],
-            'addType': [
-              'Editor'
-            ],
-            'iconClass': 'fa fa-book'
-          },
-          {
-            'type': 'TextBookUnit',
-            'label': 'Textbook Unit',
-            'isRoot': false,
-            'editable': true,
-            'childrenTypes': [
-              'TextBookUnit',
-              'Collection',
-              'Course',
-              'Resource',
-              'FocusSpot',
-              'LearningOutcomeDefinition',
-              'PracticeQuestionSet',
-              'CuriosityQuestions',
-              'MarkingSchemeRubric',
-              'ExplanationResource',
-              'ExperientialResource',
-              'eTextBook',
-              'PracticeResource',
-              'TeachingMethod',
-              'PedagogyFlow',
-              'CuriosityQuestionSet',
-              'ConceptMap',
-              'SelfAssess',
-              'ExplanationVideo',
-              'ClassroomTeachingVideo',
-              'ExplanationReadingMaterial',
-              'LearningActivity',
-              'PreviousBoardExamPapers',
-              'LessonPlanResource',
-              'TVLesson'
-            ],
-            'addType': [
-              'Editor'
-            ],
-            'iconClass': 'fa fa-folder-o'
-          },
-          {
-            'type': 'CourseUnit',
-            'label': 'Course Unit',
-            'isRoot': false,
-            'editable': true,
-            'childrenTypes': [
-              'CourseUnit',
-              'Collection',
-              'Course',
-              'Resource',
-              'FocusSpot',
-              'LearningOutcomeDefinition',
-              'PracticeQuestionSet',
-              'CuriosityQuestions',
-              'MarkingSchemeRubric',
-              'ExplanationResource',
-              'ExperientialResource',
-              'eTextBook',
-              'PracticeResource',
-              'TeachingMethod',
-              'PedagogyFlow',
-              'CuriosityQuestionSet',
-              'ConceptMap',
-              'SelfAssess',
-              'ExplanationVideo',
-              'ClassroomTeachingVideo',
-              'ExplanationReadingMaterial',
-              'LearningActivity',
-              'PreviousBoardExamPapers',
-              'LessonPlanResource',
-              'TVLesson'
-            ],
-            'addType': [
-              'Editor'
-            ],
-            'iconClass': 'fa fa-folder-o'
-          },
-          {
-            'type': 'Course',
-            'label': 'Course',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': 'Browser',
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'Collection',
-            'label': 'Collection',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'Resource',
-            'label': 'Resource',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'LessonPlan',
-            'label': 'LessonPlan',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'FocusSpot',
-            'label': 'FocusSpot',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'LearningOutcomeDefinition',
-            'label': 'LearningOutcomeDefinition',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'PracticeQuestionSet',
-            'label': 'PracticeQuestionSet',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'CuriosityQuestions',
-            'label': 'CuriosityQuestions',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'MarkingSchemeRubric',
-            'label': 'MarkingSchemeRubric',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'ExplanationResource',
-            'label': 'ExplanationResource',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'ExperientialResource',
-            'label': 'ExperientialResource',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'eTextBook',
-            'label': 'eTextBook',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'PracticeResource',
-            'label': 'PracticeResource',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'TeachingMethod',
-            'label': 'TeachingMethod',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'PedagogyFlow',
-            'label': 'PedagogyFlow',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'CuriosityQuestionSet',
-            'label': 'CuriosityQuestionSet',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'ConceptMap',
-            'label': 'ConceptMap',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'SelfAssess',
-            'label': 'SelfAssess',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'ExplanationVideo',
-            'label': 'ExplanationVideo',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'ClassroomTeachingVideo',
-            'label': 'ClassroomTeachingVideo',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'ExplanationReadingMaterial',
-            'label': 'ExplanationReadingMaterial',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'LearningActivity',
-            'label': 'LearningActivity',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'PreviousBoardExamPapers',
-            'label': 'PreviousBoardExamPapers',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'LessonPlanResource',
-            'label': 'LessonPlanResource',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          },
-          {
-            'type': 'TVLesson',
-            'label': 'TVLesson',
-            'isRoot': false,
-            'editable': false,
-            'childrenTypes': [],
-            'addType': [
-              'Browser'
-            ],
-            'iconClass': 'fa fa-file-o'
-          }
-        ]
-      },
-      'publishMode': false,
-      'isFlagReviewer': false,
-      'mode': 'Edit',
-      'contentStatus': 'draft'
-    }
-  };
-
+  config: any = editorConfig;
   constructor(public activatedRoute: ActivatedRoute, public treeService: TreeService) { }
   ngAfterViewInit() {
     let options: any = {
@@ -445,6 +38,11 @@ export class FancyTreeComponent implements AfterViewInit {
         setTimeout(() => {
           this.treeEventEmitter.emit({'type': 'nodeSelect', 'data' : data.node});
         }, 0);
+      },
+      renderNode: (event, data) => {
+        if (data.node.data.root) {
+          data.node.span.style.display = 'none';
+        }
       }
     };
     options = { ...options, ...this.options };
@@ -472,17 +70,17 @@ export class FancyTreeComponent implements AfterViewInit {
   }
 
   addChild() {
-    // const tree = $(this.tree.nativeElement).fancytree('getTree');
-    // const rootNode = $(this.tree.nativeElement).fancytree('getRootNode').getFirstChild();
-
-    // const node = tree.getActiveNode();
-    // if (this.getObjectType(node.data.objectType).editable) {
-    //   const childrenTypes = this.getObjectType(rootNode.data.objectType).childrenTypes;
-    //   this.treeService.addNode(this.getObjectType(childrenTypes[0]), {}, 'child');
-    // } else {
-    //   alert('Sorry, this operation is not allowed.');
-    // }
-    this.treeEventEmitter.emit({'type': 'addChild', 'data' : {}});
+    const tree = $(this.tree.nativeElement).fancytree('getTree');
+    const rootNode = $(this.tree.nativeElement).fancytree('getRootNode').getFirstChild();
+    const node = tree.getActiveNode();
+    if (this.getObjectType(node.data.objectType).editable) {
+      const childrenTypes = this.getObjectType(rootNode.data.objectType).childrenTypes;
+      // this.treeService.addNode(this.getObjectType(childrenTypes[0]), {}, 'child');
+      console.log((rootNode.children ? 'sibling' : 'child'));
+      this.treeEventEmitter.emit({'type': 'addChild', 'data' : (rootNode.children ? 'sibling' : 'child')});
+    } else {
+      alert('Sorry, this operation is not allowed.');
+    }
   }
 
   addSibling() {
@@ -493,6 +91,7 @@ export class FancyTreeComponent implements AfterViewInit {
     if (!node.data.root) {
       const childrenTypes = this.getObjectType(rootNode.data.objectType).childrenTypes;
       this.treeService.addNode(this.getObjectType(childrenTypes[0]), {}, 'sibling');
+      // this.treeEventEmitter.emit({'type': 'addSibling', 'data' : 'sibling'});
     } else {
       alert('Sorry, this operation is not allowed.');
     }
--- src/app/client/src/app/modules/editor/components/multiplechoice-question/multiplechoice-question.component.ts
+++ src/app/client/src/app/modules/editor/components/multiplechoice-question/multiplechoice-question.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import * as _ from 'lodash-es';
 import { UUID } from 'angular2-uuid';
 import { McqForm } from '../../../cbse-program';
+import {questionEditorConfig} from '../../editor.config';
 @Component({
   selector: 'app-multiplechoice-question',
   templateUrl: './multiplechoice-question.component.html',
@@ -15,110 +16,7 @@ export class MultiplechoiceQuestionComponent implements OnInit, OnChanges {
   public setCharacterLimit = 160;
   public setImageLimit = 1;
   public mediaArr = [];
-  public editorConfig: any = {
-    'config': {
-      'tenantName': '',
-      'assetConfig': {
-        'image': {
-          'size': '50',
-          'accepted': 'jpeg, png, jpg'
-        },
-        'video': {
-          'size': '50',
-          'accepted': 'pdf, mp4, webm, youtube'
-        }
-      },
-      'solutionType': [
-        'Video',
-        'Text & image'
-      ],
-      'No of options': 4,
-      'questionCategory': [
-        'vsa',
-        'sa',
-        'ls',
-        'mcq',
-        'curiosity'
-      ],
-      'formConfiguration': [
-        {
-          'code': 'learningOutcome',
-          'name': 'LearningOutcome',
-          'label': 'Learning Outcome',
-          'visible': true,
-          'dataType': 'list',
-          'editable': true,
-          'required': false,
-          'inputType': 'multiselect',
-          'description': 'Learning Outcomes For The Content',
-          'placeholder': 'Select Learning Outcomes'
-        },
-        {
-          'code': 'attributions',
-          'name': 'Attributions',
-          'label': 'Attributions',
-          'visible': true,
-          'dataType': 'list',
-          'editable': true,
-          'helpText': 'If you have relied on another work to create this Content, provide the name of that creator and the source of that work.',
-          'required': false,
-          'inputType': 'text',
-          'description': 'Enter Attributions',
-          'placeholder': 'Enter Attributions'
-        },
-        {
-          'code': 'copyright',
-          'name': 'Copyright',
-          'label': 'Copyright and Year',
-          'visible': true,
-          'dataType': 'text',
-          'editable': true,
-          'helpText': 'If you are an individual, creating original Content, you are the copyright holder. If you are creating Content on behalf of an organisation, the organisation may be the copyright holder. Please fill as <Name of copyright holder>, <Year of publication>',
-          'required': true,
-          'inputType': 'text',
-          'description': 'Enter Copyright and Year',
-          'placeholder': 'Enter Copyright and Year'
-        },
-        {
-          'code': 'creator',
-          'name': 'Author',
-          'label': 'Author',
-          'visible': true,
-          'dataType': 'text',
-          'editable': true,
-          'helpText': 'Provide name of creator of this Content.',
-          'required': true,
-          'inputType': 'text',
-          'description': 'Enter The Author Name',
-          'placeholder': 'Enter Author Name'
-        },
-        {
-          'code': 'license',
-          'name': 'License',
-          'label': 'License',
-          'visible': true,
-          'dataType': 'list',
-          'editable': true,
-          'helpText': 'Choose the most appropriate Creative Commons License for this Content',
-          'required': true,
-          'inputType': 'select',
-          'description': 'License For The Content',
-          'placeholder': 'Select License'
-        },
-        {
-          'code': 'contentPolicyCheck',
-          'name': 'Content Policy Check',
-          'visible': true,
-          'dataType': 'boolean',
-          'editable': false,
-          'required': true,
-          'inputType': 'checkbox'
-        }
-      ],
-      'resourceTitleLength': '200'
-    },
-    'channel': 'sunbird'
-  };
+  public editorConfig: any = questionEditorConfig;
 
   constructor() { }
 
@@ -135,7 +33,7 @@ export class MultiplechoiceQuestionComponent implements OnInit, OnChanges {
 
   initialize() {
     if (this.question && this.question.data) {
-      const { responseDeclaration, templateId, editorState } = this.question.data.model;
+      const { responseDeclaration, templateId, editorState } = this.question.data.metadata;
       const numberOfOptions = _.get(this.editorConfig.config, 'No of options');
       const options = _.map(editorState.options, option => ({ body: option.value.body }));
       const question = editorState.question;
