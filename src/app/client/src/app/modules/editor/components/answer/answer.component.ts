3afef89959d3c4a380f2db87883198b793fcd05b
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2021-01-07 14:15:08 +0530

    Issue #DP-1282 fix: code refactored for questionset editor

--- src/app/client/src/app/modules/editor/components/answer/answer.component.ts
+++ src/app/client/src/app/modules/editor/components/answer/answer.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { forkJoin, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
+import * as _ from 'lodash-es';
 @Component({
   selector: 'app-answer',
   templateUrl: './answer.component.html',
@@ -9,12 +13,27 @@ export class AnswerComponent implements OnInit {
   @Input() editorState;
   @Input() showFormError;
   @Output() editorDataOutput: EventEmitter<any> = new EventEmitter<any>();
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.editorDataHandler({body: this.editorState.answer});
   }
 
   editorDataHandler(event) {
-    this.editorDataOutput.emit(event);
+    const body = this.prepareAnwserData(event);
+    this.editorDataOutput.emit({body, mediaobj: event.mediaobj});
   }
+
+  prepareAnwserData(event) {
+    return {
+      'answer': event.body,
+      'editorState': {
+        'answer': event.body
+      },
+      'name': 'Subjective',
+      'qType': 'SA',
+      'primaryCategory': 'Subjective Question'
+    };
+  }
+
 }
--- src/app/client/src/app/modules/editor/components/options/options.component.ts
+++ src/app/client/src/app/modules/editor/components/options/options.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { ProgramTelemetryService } from '../../../program/services';
 import { ConfigService } from '@sunbird/shared';
+import * as _ from 'lodash-es';
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html',
@@ -13,21 +14,74 @@ export class OptionsComponent implements OnInit {
   @Input() telemetryPageDetails;
   @Input() questionMetaData;
   @Output() editorDataOutput: EventEmitter<any> = new EventEmitter<any>();
-  @Output() optionMedia: EventEmitter<any> = new EventEmitter<any>();
   public setCharacterLimit = 160;
-  public mediaArr = [];
   constructor(public programTelemetryService: ProgramTelemetryService,
     private configService: ConfigService) { }
 
   ngOnInit() {
+    this.editorDataHandler();
   }
 
-  editorDataHandler(event) {
-    this.editorDataOutput.emit(event);
+  editorDataHandler(event?) {
+    const body = this.prepareMcqBody(this.editorState);
+    this.editorDataOutput.emit({ body, mediaobj: event ? event.mediaobj : undefined });
   }
 
-  getMedia(media) {
-    this.optionMedia.emit(media);
+  prepareMcqBody(editorState) {
+    let metadata: any;
+    const correct_answer = editorState.answer;
+    let resindex;
+    const options = _.map(editorState.options, (opt, key) => {
+      resindex = Number(key);
+      if (Number(correct_answer) === key) {
+        return { 'answer': true, value: { 'body': opt.body, 'value': resindex } };
+      } else {
+        return { 'answer': false, value: { 'body': opt.body, 'value': resindex } };
+      }
+    });
+    metadata = {
+      'templateId': 'mcq-vertical',
+      'name': 'Multiple Choice',
+      'responseDeclaration': this.getResponseDeclaration(editorState),
+      'interactionTypes': ['choice'],
+      'interactions': this.getInteractions(editorState.options),
+      'editorState': {
+        'options': options
+      },
+      'qType': 'MCQ',
+      'primaryCategory': 'Multiple Choice Question'
+    };
+    return metadata;
+  }
+
+  getResponseDeclaration(editorState) {
+    const responseDeclaration = {
+      response1: {
+        maxScore: 1,
+        cardinality: 'single',
+        type: 'integer',
+        'correctResponse': {
+          value: editorState.answer,
+          outcomes: { 'SCORE': 1 }
+        }
+      }
+    };
+    return responseDeclaration;
+  }
+
+  getInteractions(options) {
+    let index;
+    const interactOptions = _.map(options, (opt, key) => {
+      index = Number(key);
+      return { 'label': opt.body, 'value': index };
+    });
+    const interactions = {
+      'response1': {
+        'type': 'choice',
+        'options': interactOptions
+      }
+    };
+    return interactions;
   }
 
 }
