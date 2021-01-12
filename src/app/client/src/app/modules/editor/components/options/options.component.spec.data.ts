7ffbf94e0ec2f7fca338d69114121c75608c5318
Rajnish <rajnish_d@techjoomla.com>
2021-01-12 13:55:42 +0530

    Issue DP-1039 feat: player config for question preview

--- src/app/client/src/app/modules/editor/components/options/options.component.spec.data.ts
+++ src/app/client/src/app/modules/editor/components/options/options.component.spec.data.ts
@@ -1,23 +1,4 @@
 export const mockRes = {
-    fakeParamMap: {
-        questionId: 'do_1131823509449031681174',
-        type: 'default'
-      },
-
-    McqEditorState: {
-        'question': '<p>color of sky is?</p>',
-        'options': [{
-                'body': '<p>red</p>',
-                'length': 0
-            },
-            {
-                'body': '<p>blue</p>',
-                'length': 0
-            }
-        ],
-        'answer': '0',
-        'numberOfOptions': 2
-    },
     questionOptions: [{
     'body': '<p>a1</p>',
     'length': 0
