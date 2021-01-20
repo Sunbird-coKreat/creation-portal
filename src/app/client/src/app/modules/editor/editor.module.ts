8616a65e569447b40732f435c93ae976033c59ab
Rajnish <rajnish_d@techjoomla.com>
2021-01-20 21:34:45 +0530

    Issue #DP-1039 feat: Question-Set-Editor Library integration

--- src/app/client/src/app/modules/editor/editor.module.ts
+++ src/app/client/src/app/modules/editor/editor.module.ts
@@ -1,19 +1,38 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CoreModule } from '@sunbird/core';
+import { SuiModule } from 'ng2-semantic-ui';
+import { SharedModule } from '@sunbird/shared';
 import { EditorRoutingModule } from './editor-routing.module';
+import { CommonFormElementsModule } from 'v-dynamic-forms';
 import { FancyTreeComponent, QuestionTemplateComponent, QuestionBaseComponent,
 ContentplayerPageComponent, EditorHeaderComponent,
 QuestionSetComponent, EditorBaseComponent, } from './components';
 import {CbseProgramModule} from '../cbse-program';
 import { OptionsComponent } from './components/options/options.component';
 import { AnswerComponent } from './components/answer/answer.component';
+import { QumlLibraryModule } from '@project-sunbird/sunbird-quml-player';
+import {CarouselModule} from 'ngx-bootstrap/carousel';
+
+// import { PlayerHelperModule } from '@sunbird/player-helper';
+
 @NgModule({
   declarations: [EditorHeaderComponent, QuestionSetComponent, EditorBaseComponent, FancyTreeComponent, QuestionTemplateComponent,
     QuestionBaseComponent, ContentplayerPageComponent, OptionsComponent, AnswerComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    CoreModule,
+    SuiModule,
+    ReactiveFormsModule,
     EditorRoutingModule,
+    SharedModule,
+    CommonFormElementsModule,
+    // PlayerHelperModule
     CbseProgramModule,
+    QumlLibraryModule,
+    CarouselModule.forRoot()
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ]
 })
