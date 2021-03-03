74e02eee39bb228fb4d5babaf1a547970aa36461
Bharat Kashyap <bharatkashyap@outlook.com>
2021-03-03 14:57:41 +0530

    Fix: DP-1626 Edit Blueprint visible for Draft projects of other collection category than Question Paper

--- src/app/client/src/app/modules/program/components/create-program/create-program.component.ts
+++ src/app/client/src/app/modules/program/components/create-program/create-program.component.ts
@@ -143,7 +143,6 @@ export class CreateProgramComponent implements OnInit, AfterViewInit {
     } else {
       this.initializeFormFields();
     }
-    this.fetchBlueprintTemplate();
     this.fetchFrameWorkDetails();
     this.setTelemetryStartData();
     this.pageStartTime = Date.now();
@@ -585,7 +584,7 @@ export class CreateProgramComponent implements OnInit, AfterViewInit {
   }
 
   fetchBlueprintTemplate(): void {
-    this.programsService.getCollectionCategoryDefinition(this.selectedTargetCollection || 'Question paper', this.userprofile.rootOrgId).subscribe(res => {
+    this.programsService.getCollectionCategoryDefinition(this.selectedTargetCollection, this.userprofile.rootOrgId).subscribe(res => {
       let templateDetails = res.result.objectCategoryDefinition;
       if(templateDetails && templateDetails.forms) {         
         this.blueprintTemplate = templateDetails.forms.blueprintCreate;                             
@@ -652,8 +651,9 @@ export class CreateProgramComponent implements OnInit, AfterViewInit {
 
       this.createProgramForm = this.sbFormBuilder.group(obj);
       this.defaultContributeOrgReviewChecked = _.get(this.programDetails, 'config.defaultContributeOrgReview') ? false : true;
+      this.fetchBlueprintTemplate();
       this.showProgramScope = false;
-      this.showTextBookSelector = false;
+      this.showTextBookSelector = false;      
     }
 
     this.showLoader = false;
