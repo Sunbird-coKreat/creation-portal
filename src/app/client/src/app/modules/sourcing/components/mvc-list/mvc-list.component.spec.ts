bea1e9ebdb9e2899171d574e4cba9f9392b0bb41
Rajnish <rajnish_d@techjoomla.com>
2021-03-22 09:25:49 +0530

    Issue #SB-22932 feat: Improve coverage of unit tests - Sourcing portal

--- src/app/client/src/app/modules/sourcing/components/mvc-list/mvc-list.component.spec.ts
+++ src/app/client/src/app/modules/sourcing/components/mvc-list/mvc-list.component.spec.ts
@@ -1,19 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SuiPopupModule } from 'ng2-semantic-ui';
 import { MvcListComponent } from './mvc-list.component';
-import { DebugElement } from '@angular/core';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ResourceService, ConfigService, InterpolatePipe, BrowserCacheTtlService } from '@sunbird/shared';
+import { ProgramTelemetryService } from '../../../program/services';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CacheService } from 'ng2-cache-service';
+import { APP_BASE_HREF } from '@angular/common';
+import { mockData} from './mvc-list.component.spec.data';
 
-xdescribe('MvcListComponent', () => {
+describe('MvcListComponent', () => {
   let component: MvcListComponent;
   let fixture: ComponentFixture<MvcListComponent>;
   let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [SuiPopupModule, RouterTestingModule, FormsModule, ReactiveFormsModule],
-      declarations: [ MvcListComponent ]
+      imports: [SuiPopupModule, RouterTestingModule, FormsModule, ReactiveFormsModule, HttpClientTestingModule],
+      declarations: [ MvcListComponent, InterpolatePipe ],
+      providers:[ResourceService, ConfigService, ProgramTelemetryService, CacheService, BrowserCacheTtlService,
+        {provide: APP_BASE_HREF, useValue: '/'}],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -33,21 +42,30 @@ xdescribe('MvcListComponent', () => {
     expect(component.addToLibraryBtnVisibility).toBe(false);
   });
 
-  it('should show #addToLibrary button after calls ngOnInit', () => {
-    component.ngOnInit();
-    expect(component.addToLibraryBtnVisibility).toBe(true);
-  });
-
   it('#onContentChange() should emit event with #contentId', () => {
-    const contentId = 'do_1130454643544227841156';
+    const contentData = { name: 'DummyName', identifier: 'do_12345'}
     spyOn(component.contentChangeEvent, 'emit');
-    component.onContentChange(contentId);
+    component.onContentChange(contentData);
     expect(component.contentChangeEvent.emit).toHaveBeenCalledWith({
-      contentId: contentId
+      content: contentData
     });
   });
 
-  it('should show #addToLibrary button when clicked (onContentChange)', () => {
+  it('should call onShowAddedContentChange to show the added content', () => {
+    component.showAddedContent = true;
+    spyOn(component.moveEvent, 'emit').and.returnValue(mockData.showAddedContent);
+    component.onShowAddedContentChange();
+    expect(component.moveEvent.emit).toHaveBeenCalledWith(mockData.showAddedContent);
+  });
+
+  it('should call onShowAddedContentChange to hide added content', () => {
+    component.showAddedContent = false;
+    spyOn(component.moveEvent, 'emit').and.returnValue(mockData.hideAddedContent);
+    component.onShowAddedContentChange();
+    expect(component.moveEvent.emit).toHaveBeenCalledWith(mockData.hideAddedContent);
+  });
+
+  xit('should show #addToLibrary button when clicked (onContentChange)', () => {
     const contentId = 'do_1130454643544227841156';
     spyOn(component.contentChangeEvent, 'emit');
     component.onContentChange(contentId);
@@ -62,6 +80,13 @@ xdescribe('MvcListComponent', () => {
     expect(component.moveEvent.emit).toHaveBeenCalledWith({
       action: 'beforeMove'
     });
+  });
+
+  it('should call updateContentViewed to hide added content', () => {
+    component.inViewLogs = [];
+    spyOn(component.moveEvent, 'emit').and.returnValue(mockData.contentVisits);
+    component.updateContentViewed();
+    expect(component.moveEvent.emit).toHaveBeenCalledWith(mockData.contentVisits);
   });
 
   it('#changeFilter() should emit #showFilter event', () => {
