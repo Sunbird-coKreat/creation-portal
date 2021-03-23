39b01d5d5b7622d4813ebedd16fa686d05cca74d
vaishali <vaishali_k@techjoomla.com>
2021-03-23 17:07:48 +0530

    Issue #SB-23783 fix:Approve/Reject button is not available for sourcing org reviewer for contribution date ended Project.

--- src/app/client/src/app/modules/sourcing/components/question-list/question-list.component.ts
+++ src/app/client/src/app/modules/sourcing/components/question-list/question-list.component.ts
@@ -509,7 +509,7 @@ export class QuestionListComponent implements OnInit, AfterViewInit, OnDestroy {
      // tslint:disable-next-line:max-line-length
     this.visibility['showEdit'] = submissionDateFlag && this.canEdit();
     // tslint:disable-next-line:max-line-length
-    this.visibility['showSourcingActionButtons'] = submissionDateFlag && this.canSourcingReviewerPerformActions();
+    this.visibility['showSourcingActionButtons'] = this.canSourcingReviewerPerformActions();
   }
 
   canDeleteQuestion() {
@@ -563,7 +563,8 @@ export class QuestionListComponent implements OnInit, AfterViewInit, OnDestroy {
     && !this.resourceDetails.sampleContent === true && this.resourceStatus === 'Live'
     && this.userService.userid !== this.resourceDetails.createdBy
     && this.resourceStatus === 'Live' && !this.sourcingReviewStatus &&
-    (this.originCollectionData.status === 'Draft' && this.selectedOriginUnitStatus === 'Draft'));
+    (this.originCollectionData.status === 'Draft' && this.selectedOriginUnitStatus === 'Draft')
+    && this.programsService.isProjectLive(this.programContext));
   }
 
   canEditContentTitle() {
