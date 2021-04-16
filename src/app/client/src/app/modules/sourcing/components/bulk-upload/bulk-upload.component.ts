5b8c7075563d29120da33bc67211baf37a51fda6
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2021-04-16 12:48:39 +0530

    Issue #SB-23923 fix: Creation portal code refactoring

--- src/app/client/src/app/modules/sourcing/components/bulk-upload/bulk-upload.component.ts
+++ src/app/client/src/app/modules/sourcing/components/bulk-upload/bulk-upload.component.ts
@@ -68,6 +68,7 @@ export class BulkUploadComponent implements OnInit {
   public telemetryInteractPdata: any;
   public telemetryInteractObject: any;
   public userProfile: IUserProfile;
+  
   public unsubscribe = new Subject<void>();
   constructor(
     private userService: UserService,
