ad79f87503305a1c4063bf01b591c49b0b8490d1
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2021-04-16 13:59:33 +0530

    Issue #SB-23923 fix: Creation portal code refactoring

---
+++ src/app/client/src/app/modules/program/components/list-contributor-textbooks/list-contributor-textbooks.component.spec.ts
@@ -0,0 +1,94 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListContributorTextbooksComponent } from './list-contributor-textbooks.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { DaysToGoPipe } from '@sunbird/shared-feature';
+import { SuiModule } from 'ng2-semantic-ui';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TelemetryModule } from '@sunbird/telemetry';
+import { SharedModule } from '@sunbird/shared';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { DynamicModule } from 'ng-dynamic-component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {APP_BASE_HREF, DatePipe} from '@angular/common';
+import * as SpecData from './list-contributor-textbooks.spec.data';
+import { CollectionHierarchyService } from '../../../sourcing/services/collection-hierarchy/collection-hierarchy.service';
+import { throwError } from 'rxjs';
+import { UserService} from '@sunbird/core';
+describe('ListContributorTextbooksComponent', () => {
+  let component: ListContributorTextbooksComponent;
+  let fixture: ComponentFixture<ListContributorTextbooksComponent>;
+  class RouterStub {
+    navigate = jasmine.createSpy('navigate');
+  }
+const errorInitiate = false;
+  const userServiceStub = {
+    get() {
+      if (errorInitiate) {
+        return throwError ({
+          result: {
+            responseCode: 404
+          }
+        });
+      }
+    },
+    userid: SpecData.userProfile.userId,
+    userProfile : SpecData.userProfile
+  };
+  const fakeActivatedRoute = {
+    snapshot: {
+      params: {
+        programId: '6835f250-1fe1-11ea-93ea-2dffbaedca40'
+      },
+      data: {
+        telemetry: {
+          env: 'programs'
+        }
+      }
+    },
+  };
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+     imports: [
+            DynamicModule,
+            SuiModule,
+            SharedModule.forRoot(),
+            ReactiveFormsModule,
+            FormsModule,
+            TelemetryModule.forRoot(),
+            HttpClientTestingModule,
+            RouterModule.forRoot([])
+        ],
+      declarations: [ ListContributorTextbooksComponent, DaysToGoPipe],
+      providers: [
+        {
+            provide: Router,
+            useClass: RouterStub
+          },
+          {
+            provide: ActivatedRoute,
+            useValue: fakeActivatedRoute
+          },
+          {
+            provide: UserService,
+            useValue: userServiceStub
+          },
+          {provide: APP_BASE_HREF, useValue: '/'},
+          DatePipe,
+          CollectionHierarchyService
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListContributorTextbooksComponent);
+    component = fixture.componentInstance;
+    // fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
