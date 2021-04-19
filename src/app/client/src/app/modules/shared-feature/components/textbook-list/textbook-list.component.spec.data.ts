15d07479b5dc0e2d3e1b1b853d2e42cea65811d6
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2021-04-19 15:24:12 +0530

    Issue #SB-23923 fix: Creation portal code refactoring

--- src/app/client/src/app/modules/shared-feature/components/textbook-list/textbook-list.component.spec.data.ts
+++ src/app/client/src/app/modules/shared-feature/components/textbook-list/textbook-list.component.spec.data.ts
@@ -12,6 +12,7 @@ export const addParticipentResponseSample = {
     ver: '1.0'
   };
 
+
   export const userProfile = {
     'userId': '874ed8a5-782e-4f6c-8f36-e0288455901e',
     'userRegData': {},
