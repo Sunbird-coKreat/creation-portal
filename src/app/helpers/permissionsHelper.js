092ac9f31f8f403716b263bb9011adff1d8a28e3
Venkateshwaran Selvaraj <venkat@sanketika.in>
2021-07-16 18:03:32 +0530

    Issue #SB-25553 fix: adding api whitelist

--- src/app/helpers/permissionsHelper.js
+++ src/app/helpers/permissionsHelper.js
@@ -187,7 +187,7 @@ let PERMISSIONS_HELPER = {
     const userRegData = {};
     var userId = reqObj.session.userId
     const option = {
-      url: `${openSaberServiceUrl}/reg/search`,
+      url: `${openSaberServiceUrl}/search`,
       headers: {
         'x-msgid': uuidv1(),
         'ts': dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss:lo'),
