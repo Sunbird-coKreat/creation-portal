756e29a93094c2c11c0d4e045b6ce77a7b99e251
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2021-08-09 17:42:31 +0530

    #SB-25319 feat: Editors in Sourcing Solution should have minimal requirement

--- src/app/client/src/app/modules/sourcing/components/content-editor/content-editor.component.spec.ts
+++ src/app/client/src/app/modules/sourcing/components/content-editor/content-editor.component.spec.ts
@@ -126,6 +126,7 @@ describe('ContentEditorComponent', () => {
     component.getFormData(formInput);
     expect(component.formInputData).toBe(formInput);
   });
+  
   it ('#handleBack() should call handleBack and emit header event', () => {
     spyOn(component['programsService'],'emitHeaderEvent' ).and.callThrough();
     spyOn(component['programStageService'],'removeLastStage' ).and.callThrough();
