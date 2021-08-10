505a1f1542fdc97bfaf392cc1793f11c87f1ec45
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2021-08-10 18:42:29 +0530

    #SB-25319 feat: updated test cases

--- src/app/client/src/app/modules/shared/services/util/util.service.spec.ts
+++ src/app/client/src/app/modules/shared/services/util/util.service.spec.ts
@@ -126,8 +126,7 @@ describe('UtilService', () => {
     expect(service.translateLabel).toBeTruthy();
   }));
 
-
-  xit('should call convertSelectedOption method', inject([UtilService], (service: UtilService) => {
+ it('should call convertSelectedOption method', inject([UtilService], (service: UtilService) => {
     spyOn(service, 'convertSelectedOption').and.callThrough();
     service.convertSelectedOption(null, null, null, null);
     expect(service.convertSelectedOption).toBeTruthy();
