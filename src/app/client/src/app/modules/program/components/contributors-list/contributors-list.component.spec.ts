2d8a8e4bf8780626141d3985f6cd8947db397e22
Amol Ghatol <amol_g@techjoomla.com>
2021-08-10 12:05:31 +0530

    Issue #SB-25924 feat: Projects with Invite Only option for contributions

--- src/app/client/src/app/modules/program/components/contributors-list/contributors-list.component.spec.ts
+++ src/app/client/src/app/modules/program/components/contributors-list/contributors-list.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ContributorsListComponent } from './contributors-list.component';
+import { TelemetryService } from './../../../telemetry/services/telemetry/telemetry.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SharedModule } from '@sunbird/shared';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CoreModule } from '@sunbird/core';
+import { TelemetryModule } from '@sunbird/telemetry';
+import * as _ from 'lodash-es';
+import {  FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SuiModule } from 'ng2-semantic-ui-v9';
+import { RegistryService, ProgramsService } from '@sunbird/core';
+import { ResourceService, PaginationService, ConfigService } from '@sunbird/shared';
+import { SourcingService } from './../../../sourcing/services';
 
 describe('ContributorsListComponent', () => {
   let component: ContributorsListComponent;
@@ -8,9 +19,11 @@ describe('ContributorsListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContributorsListComponent ]
+      imports: [ReactiveFormsModule, HttpClientTestingModule, FormsModule, CoreModule, TelemetryModule, RouterTestingModule, SharedModule.forRoot(), SuiModule],
+      declarations: [ContributorsListComponent],
+      providers: [TelemetryService, RegistryService, ProgramsService, ResourceService, PaginationService, ConfigService, SourcingService],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +35,12 @@ describe('ContributorsListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should call the getOrgList', () => {
+    spyOn(component, 'ngOnInit');
+    component.ngOnInit();
+    expect(component).toBeTruthy();
+    expect(component.ngOnInit).toHaveBeenCalled();
+  });
+
 });
