c3b91e9446996c49032d7bbefba4d8314cd708eb
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2021-08-25 16:59:24 +0530

    Issue #SB-24542 feat: added usage details section

--- src/app/client/src/app/modules/contribute/components/my-content/my-content.component.ts
+++ src/app/client/src/app/modules/contribute/components/my-content/my-content.component.ts
@@ -38,6 +38,10 @@ export class MyContentComponent implements OnInit, AfterViewInit {
     published: 0,
     notPublished: 0
   };
+  public usageDetailsCount: any = {
+    totalPlays: 0,
+    averageRating: 0
+  };
   public direction = 'asc';
   public sortColumn = '';
   public selectedContributionDetails: any;
@@ -180,6 +184,14 @@ export class MyContentComponent implements OnInit, AfterViewInit {
           notPublished: this.getNotPublishedContentCount(obj[value.frameworkType], value)
         };
       }
+
+      if (value.isPublished) {
+        // tslint:disable-next-line:max-line-length
+        this.usageDetailsCount.totalPlays = value.me_totalPlaySessionCount ? (this.usageDetailsCount.totalPlays + this.getCountData(value, 'me_totalPlaySessionCount')) : this.usageDetailsCount.totalPlays;
+        // tslint:disable-next-line:max-line-length
+        this.usageDetailsCount.averageRating = value.me_averageRating ? (value.me_averageRating + this.usageDetailsCount.averageRating) : this.usageDetailsCount.averageRating;
+      }
+
     });
     this.contributionDetails = obj;
     this.setContentCount(this.totalPublishedContent, (this.totalContent - this.totalPublishedContent));
