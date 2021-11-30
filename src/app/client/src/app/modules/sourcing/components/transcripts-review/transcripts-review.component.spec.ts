19dbdc8ffd2c250ef94c5b0f984289417b0085cc
Rajnish Dargan <rajnish.code30@gmail.com>
2021-11-30 13:27:18 +0530

    Issue #SB-26446 feat: transcript test cases

--- src/app/client/src/app/modules/sourcing/components/transcripts-review/transcripts-review.component.spec.ts
+++ src/app/client/src/app/modules/sourcing/components/transcripts-review/transcripts-review.component.spec.ts
@@ -1,14 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranscriptsReviewComponent } from './transcripts-review.component';
-import { ResourceService, ConfigService } from '@sunbird/shared';
-xdescribe('TranscriptsReviewComponent', () => {
+import { ResourceService, ConfigService, BrowserCacheTtlService } from '@sunbird/shared';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CacheService } from 'ng2-cache-service';
+describe('TranscriptsReviewComponent', () => {
   let component: TranscriptsReviewComponent;
   let fixture: ComponentFixture<TranscriptsReviewComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ TranscriptsReviewComponent ],
-      providers: [ResourceService, ConfigService]
+      providers: [ResourceService, ConfigService, CacheService, BrowserCacheTtlService]
     })
     .compileComponents();
   }));
@@ -21,5 +24,32 @@ xdescribe('TranscriptsReviewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('ngOnInit should call setTranscripts', () => {
+    spyOn(component, 'setTranscripts').and.callFake(() => {});
+    spyOn(component, 'ngOnInit').and.callThrough();
+    component.ngOnInit();
+    expect(component.setTranscripts).toHaveBeenCalled();
+  });
+
+  it('setTranscripts should return transcripts with file name', () => {
+    component.contentTranscript = [
+      {
+          "identifier": "do_11341782099017728011533",
+          "language": "Bengali",
+          "artifactUrl": "https://dockstorage.blob.core.windows.net/sunbird-content-dock/content/assets/do_11341782099017728011533/srt-e.srt"
+      }];
+    spyOn(component, 'setTranscripts').and.callThrough();
+    component.setTranscripts();
+    expect(component.transcripts).toBeDefined();
+  });
+
+  it('downloadFile should call window.open', () => {
+    spyOn(window, 'open').and.callFake(() => {});
+    spyOn(component, 'downloadFile').and.callThrough();
+    // spyOn(component, 'ngOnInit').and.callThrough();
+    component.downloadFile("https://dockstorage.blob.core.windows.net/sunbird-content-dock/content/assets/do_11341782099017728011533/srt-e.srt");
+    expect(window.open).toHaveBeenCalled();
   });
 });
