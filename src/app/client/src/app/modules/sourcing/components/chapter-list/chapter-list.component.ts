c70480eb2f01fd56fcb6701d45924d63482c7def
Manoj Singh Rawat <manojrawat1502@gmail.com>
2021-12-31 16:45:29 +0530

    feat: dynamic header added in question

--- src/app/client/src/app/modules/sourcing/components/chapter-list/chapter-list.component.ts
+++ src/app/client/src/app/modules/sourcing/components/chapter-list/chapter-list.component.ts
@@ -157,11 +157,6 @@ export class ChapterListComponent implements OnInit, OnChanges, OnDestroy, After
       this.state.stages = state.stages;
       this.changeView();
     });
-    this.helperService.getDynamicHeaders(this.configUrl).subscribe((state: any) => {
-      if(_.has(state, "headers")){
-        this.dynamicHeaders = state.headers;
-      }
-    });
     this.currentStage = 'chapterListComponent';
     this.sessionContext = _.get(this.chapterListComponentInput, 'sessionContext');
     this.programContext = _.get(this.chapterListComponentInput, 'programContext');
@@ -196,6 +191,11 @@ export class ChapterListComponent implements OnInit, OnChanges, OnDestroy, After
     if ( _.isUndefined(this.sessionContext.topicList)) {
         this.fetchFrameWorkDetails();
     }
+    this.helperService.getDynamicHeaders(this.configUrl, this.projectTargetType).subscribe((state: any) => {
+      if (_.has(state, 'headers')) {
+        this.dynamicHeaders = state.headers;
+      }
+    });
     this.getCollectionCategoryDefinition();
     /**
      * @description : this will fetch question Category configuration based on currently active route
