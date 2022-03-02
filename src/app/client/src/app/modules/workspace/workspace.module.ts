dd6c27b6abd49ed9c7082b9b2ec98196cca76d4e
Amol Ghatol <amol_g@techjoomla.com>
2022-03-02 10:19:25 +0530

    Issue #SB-28619 chore: Added new Workspace module

--- src/app/client/src/app/modules/workspace/workspace.module.ts
+++ src/app/client/src/app/modules/workspace/workspace.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkspaceComponent } from './components/workspace/workspace.component';
 import { WorkspaceRoutingModule } from './workspace-routing.module';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [WorkspaceComponent],
   imports: [
     WorkspaceRoutingModule,
-    CommonModule
+    CommonModule,
+    MatSliderModule
   ]
 })
 export class WorkspaceModule { }
