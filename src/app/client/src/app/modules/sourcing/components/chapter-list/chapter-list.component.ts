19b8f081ab6ea0e942883b5766e64841529e4b86
vaishali <vaishali_k@techjoomla.com>
2022-04-05 17:49:33 +0530

    issue #0 fix: editable fields not shown on chapter list

--- src/app/client/src/app/modules/sourcing/components/chapter-list/chapter-list.component.ts
+++ src/app/client/src/app/modules/sourcing/components/chapter-list/chapter-list.component.ts
@@ -533,7 +533,7 @@ export class ChapterListComponent implements OnInit, OnChanges, OnDestroy, After
         const formData = this.programsService.getformConfigData(this.programContext.rootorg_id, 'framework', contextType, 'content', 'create', _.get(this.sessionContext, 'targetCollectionPrimaryCategory'));
         if (_.has(formData, "result.data.properties") && this.frameworkService.orgFrameworkCategories) {
           _.forEach(this.frameworkService.orgFrameworkCategories, (orgFrameworkCategory) => {
-            _.forEach(formData.properties, (prop) => {
+            _.forEach(_.get(formData, "result.data.properties"), (prop) => {
               if(prop.code == orgFrameworkCategory.code && prop.editable){
                 this.tags.push(prop.code);
               }
