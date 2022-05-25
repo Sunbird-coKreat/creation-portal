28ae334b8083345d0c44d44a3e9e9b1db260cf6c
arun1205 <arun.kumar@tarento.com>
2022-05-25 13:42:10 +0530

    race fix for all tests

--- src/app/client/src/test.ts
+++ src/app/client/src/test.ts
@@ -19,9 +19,9 @@ getTestBed().initTestEnvironment(
 
 // then we find all the tests.
 const filterRegExp = (tags) ? new RegExp(tags, 'g') : /\.spec\.ts$/,
-  //  context = require.context('./', true, /\.spec\.ts$/),
+    context = require.context('./', true, /\.spec\.ts$/),
   //  context = require.context('./app/modules/program/components/', true, /create-program.component.spec\.ts$/),
-      context = require.context('./app/modules/program/components/', true, /\.spec\.ts$/),
+  //  context = require.context('./app/modules/program/components/', true, /\.spec\.ts$/),
     specFiles = context.keys().filter(path => filterRegExp.test(path));
 // and load the modules.
 specFiles.map(context);
