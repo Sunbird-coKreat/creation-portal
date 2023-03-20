1115c9bd174f614ddb7bf6e9a369688780a06a11
Arun Kumar <arun.kumar@tarento.com>
2023-03-20 10:13:08 +0530

    updated package json

--- src/app/helpers/environmentVariablesHelper.js
+++ src/app/helpers/environmentVariablesHelper.js
@@ -15,20 +15,17 @@ let envVariables = {
 
 
   // Application Start-up - Hosts and PORT Configuration
-  PORTAL_PORT: env.sunbird_port || 3000,
-  LEARNER_URL: env.sunbird_learner_player_url || 'https://staging.open-sunbird.org/api/',
-  CONTENT_URL: env.sunbird_content_player_url || 'https://staging.open-sunbird.org/api/',
-  CONTENT_PROXY_URL: env.sunbird_content_proxy_url || 'https://staging.open-sunbird.org',
+  PORTAL_PORT: env.sunbird_port || 3001,
+  LEARNER_URL: env.sunbird_learner_player_url || 'https://staging.sunbirded.org/api/',
+  CONTENT_PROXY_URL: env.sunbird_content_proxy_url || 'https://dockstaging.sunbirded.org',
   PORTAL_REALM: env.sunbird_portal_realm || 'sunbird',
-  PORTAL_AUTH_SERVER_URL: env.sunbird_portal_auth_server_url || 'https://staging.open-sunbird.org/auth',
+  PORTAL_AUTH_SERVER_URL: env.sunbird_portal_auth_server_url || 'https://staging.sunbirded.org/auth',
   PORTAL_AUTH_SERVER_CLIENT: env.sunbird_portal_auth_server_client || 'portal',
-  PORTAL_API_AUTH_TOKEN: env.dock_api_auth_token,
-  SUNBIRD_PORTAL_API_AUTH_TOKEN: env.sunbird_api_auth_token,
-  PORTAL_ECHO_API_URL: env.sunbird_echo_api_url || 'https://staging.open-sunbird.org/api/echo/',
-  CONFIG_URL: env.sunbird_config_service_url || 'https://staging.open-sunbird.org/api/config/',
+  PORTAL_ECHO_API_URL: env.sunbird_echo_api_url || 'https://staging.sunbirded.org/api/echo/',
+  CONFIG_URL: env.sunbird_config_service_url || 'https://staging.sunbirded.org/api/config/',
   EKSTEP_ENV: env.ekstep_env || 'qa',
   DEVICE_REGISTER_API: process.env.sunbird_device_register_api || 'https://api.open-sunbird.org/v3/device/register/',
-  DEVICE_PROFILE_API: process.env.sunbird_device_profile_api || 'https://staging.open-sunbird.org/api/v3/device/profile/',
+  DEVICE_PROFILE_API: process.env.sunbird_device_profile_api || 'https://staging.sunbirded.org/api/v3/device/profile/',
   sunbird_theme: env.sunbird_theme || 'default',
   BUILD_NUMBER: packageObj.version + '.' + packageObj.buildHash,
   sunbird_portal_log_level: env.sunbird_portal_log_level || 'debug',
@@ -42,20 +39,13 @@ let envVariables = {
   CRYPTO_ENCRYPTION_KEY_EXTERNAL: env.crypto_encryption_key_external || '030702me8696b8ee2aa71x9n13l4251e',
   LOG_FINGERPRINT_DETAILS: env.sunbird_log_fingerprint_details || 'true',
   SUNBIRD_PORTAL_BASE_URL: env.sunbird_portal_base_url,
-  DOCK_CHANNEL: env.dock_channel || 'sunbird',
+
   sunbird_device_api: env.sunbird_device_api || 'https://staging.ntp.net.in/api/',
-  dock_api_call_log_status: env.dock_api_call_log_status || false,
   SUNBIRD_PORTAL_URL: env.sunbird_portal_url,
-  SUNBIRD_LEARNER_URL: env.sunbird_learner_url,
-  DOCK_PROGRAM_SERVICE_URL: env.dock_program_service_url,
-  DOCK_QUESTIONSET_ENABLE: env.dock_questionSet_enable || 'true',
-  DOCK_SMS_URL: env.dock_sms_url || 'https://vdn.diksha.gov.in',
-  DOCK_DEFAULT_FILE_SIZE: env.dock_default_file_size || 150,
-  DOCK_DEFAULT_VIDEO_SIZE: env.dock_default_video_size || 15000,
+  SUNBIRD_LEARNER_URL: env.sunbird_learner_url || 'https://staging.ntp.net.in/api/',
   SUNBIRD_PROTO: env.sunbird_base_proto,
-  OPENSABER_SERVICE_URL: env.opensaber_service_url || 'http://opensaber-service:8080',
   USE_SUNBIRD_KONG_TOKEN: env.use_sunbird_kong_token || 'true',
-  sunbird_kong_refresh_token_api: env.sunbird_kong_refresh_token_api || '',
+  sunbird_kong_refresh_token_api: env.sunbird_kong_refresh_token_api || "https://staging.sunbirded.org/auth/v1/refresh/token",
   LOCAL_DEVELOPMENT: env.local_development || false,
   // generic editor question set and coleections children contents limit
   SUNBIRD_QUESTIONSET_CHILDREN_LIMIT: env.sunbird_questionset_children_limit || 500,
@@ -67,7 +57,7 @@ let envVariables = {
   SUNBIRD_BULK_UPLOAD_DESC_LENGTH: env.sunbird_bulk_upload_description_length || '500',
   sunbird_accessibility_guidelines_url: env.sunbird_accessibility_guidelines_url || '',
   ALLOWED_FRAMEWORK_TYPES: env.allowed_framework_types || 'K-12,TPD',
-  DOCK_INTERACTIVE_VIDEO_QSET_CATEGORY: env.dock_interactive_video_qset_category || 'Interactive Video Question Set',
+
   // TTL and Intervals
   CONFIG_REFRESH_INTERVAL: env.config_refresh_interval || 10,
   PORTAL_API_CACHE_TTL: env.sunbird_api_response_cache_ttl || '600',
@@ -114,11 +104,6 @@ let envVariables = {
   sunbird_google_captcha_site_key: env.sunbird_google_captcha_site_key,
   google_captcha_private_key: env.google_captcha_private_key,
 
-
-  // Android Configuration
-  ANDROID_APP_URL: env.sunbird_android_app_url || 'http://www.sunbird.org',
-
-
   // BLOB and Storage Configuration
   PORTAL_CLOUD_STORAGE_URL: env.portal_cloud_storage_url,
   CACHE_STORE: env.sunbird_cache_store || 'memory',
@@ -136,11 +121,11 @@ let envVariables = {
 
 
   //Cloud Agnostic Changes
-  cloud_storage_provider: env.sunbird_cloud_storage_provider, // azure, aws or gcloud
-  sunbird_cloud_storage_key: env.sunbird_cloud_storage_key,
-  sunbird_cloud_storage_secret: env.sunbird_cloud_storage_secret,
+  cloud_storage_provider: env.sunbird_cloud_storage_provider || 'azure', // azure, aws or gcloud
+  sunbird_cloud_storage_key: env.sunbird_cloud_storage_key || "AKIASM3PAPX64TA2HUOE",
+  sunbird_cloud_storage_secret: env.sunbird_cloud_storage_secret  || "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",
   sunbird_cloud_report_container: env.sunbird_cloud_report_container  || 'reports',
-  sunbird_cloud_storage_region: env.sunbird_cloud_storage_region,
+  sunbird_cloud_storage_region: env.sunbird_cloud_storage_region || "ap-south-1",
   sunbird_cloud_storage_container: env.sunbird_cloud_storage_container || '',
   sunbird_gcloud_project_id: env.sunbird_gcloud_project_id || '',
 
@@ -155,11 +140,8 @@ let envVariables = {
   content_Service_Local_BaseUrl: env.sunbird_content_service_local_base_url || 'http://content-service:5000',
   CONTENT_SERVICE_UPSTREAM_URL: env.sunbird_content_service_upstream_url || 'http://localhost:5000/',
   LEARNER_SERVICE_UPSTREAM_URL: env.sunbird_learner_service_upstream_url || 'http://localhost:9000/',
-  DATASERVICE_URL: env.sunbird_dataservice_url || 'https://staging.open-sunbird.org/api/',
-  PORTAL_EXT_PLUGIN_URL: process.env.sunbird_ext_plugin_url || 'http://player_player:3000/plugin/',
-  kp_content_service_base_url: env.sunbird_kp_content_service_base_url || 'https://dock.sunbirded.org/api/',
-  kp_learning_service_base_url: env.sunbird_kp_learning_service_base_url || 'https://dock.sunbirded.org/api/',
-  kp_assessment_service_base_url: env.sunbird_kp_assessment_service_base_url || 'https://dock.sunbirded.org/api/',
+  DATASERVICE_URL: env.sunbird_dataservice_url || 'https://staging.sunbirded.org/api/',
+  PORTAL_EXT_PLUGIN_URL: process.env.sunbird_ext_plugin_url || 'http://player_player:3001/plugin/',
 
 
   // Health Checks Configuration
@@ -167,16 +149,6 @@ let envVariables = {
   sunbird_learner_service_health_status: 'true',
   sunbird_content_service_health_status: 'true',
   sunbird_portal_cassandra_db_health_status: 'true',
-
-
-  // Desktop App Configuration
-  sunbird_portal_offline_tenant: env.sunbird_portal_offline_tenant,
-  sunbird_portal_offline_supported_languages: env.sunbird_portal_offline_supported_languages,
-  sunbird_portal_offline_app_release_date: env.sunbird_portal_offline_app_release_date,
-  sunbird_portal_offline_app_version: env.sunbird_portal_offline_app_version,
-  sunbird_portal_offline_app_download_url: env.sunbird_portal_offline_app_download_url,
-  DESKTOP_APP_STORAGE_URL: env.desktop_app_storage_url,
-
 
   // CDN Configuration
   PORTAL_CDN_URL: env.sunbird_portal_cdn_url || '',
@@ -186,10 +158,28 @@ let envVariables = {
 
   // Kafka Configuration
   sunbird_processing_kafka_host: process.env.sunbird_processing_kafka_host,
-  sunbird_sso_kafka_topic: process.env.sunbird_sso_kafka_topic
+  sunbird_sso_kafka_topic: process.env.sunbird_sso_kafka_topic,
+
+  // Dock Configuration
+  SUNBIRD_PORTAL_API_AUTH_TOKEN: env.sunbird_api_auth_token || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJKa21PZDJGMzdmak1Vb3ZsdnBZclRCN3ZwTHlTV2dwWiJ9.Te7nCwnpPx5mx0P7cnveXtErMMSuarqALdiS1PFanW0",
+  CONTENT_URL: env.sunbird_content_player_url || 'https://dockstaging.sunbirded.org/api/',
+  PORTAL_API_AUTH_TOKEN: env.dock_api_auth_token || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJlZTQyM2UwY2NmY2I0N2I0YmU0NzYyZmJlM2I0MWM2YyJ9.oTnuTv_jY2XJmM2nvjLOg8yeQ1MWmGLFnF5NXmB2KOE',
+  DOCK_CHANNEL: env.dock_channel || 'sunbird',
+  OPENSABER_SERVICE_URL: env.opensaber_service_url || 'https://dock.sunbirded.org/api/',
+  DOCK_PROGRAM_SERVICE_URL: env.dock_program_service_url || 'https://dock.sunbirded.org/api/',
+  DOCK_QUESTIONSET_ENABLE: env.dock_questionSet_enable || 'true',
+  DOCK_SMS_URL: env.dock_sms_url || 'https://vdn.diksha.gov.in',
+  DOCK_DEFAULT_FILE_SIZE: env.dock_default_file_size || 150,
+  DOCK_DEFAULT_VIDEO_SIZE: env.dock_default_video_size || 15000,
+  dock_api_call_log_status: env.dock_api_call_log_status || false,
+  DOCK_INTERACTIVE_VIDEO_QSET_CATEGORY: env.dock_interactive_video_qset_category || 'Interactive Video Question Set',
+
+  // Dock Knolg Service Configuration
+  kp_content_service_base_url: env.sunbird_kp_content_service_base_url || 'https://dock.sunbirded.org/api/',
+  kp_learning_service_base_url: env.sunbird_kp_learning_service_base_url || 'https://dock.sunbirded.org/api/',
+  kp_assessment_service_base_url: env.sunbird_kp_assessment_service_base_url || 'https://dock.sunbirded.org/api/',
 }
 
-envVariables.PORTAL_CASSANDRA_URLS = (env.sunbird_cassandra_urls && env.sunbird_cassandra_urls !== '')
-  ? env.sunbird_cassandra_urls.split(',') : ['localhost']
+envVariables.PORTAL_CASSANDRA_URLS = ["localhost:9042"];
 
 module.exports = envVariables
