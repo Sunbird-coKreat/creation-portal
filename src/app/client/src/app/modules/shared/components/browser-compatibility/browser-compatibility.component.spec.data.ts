abab821f200b3d5baf41e8bc283cc81cfc4f8337
shubhamBansalCockrate <shubham.bansal@tarento.com>
2023-07-04 12:07:57 +0530

    test case fix

--- src/app/client/src/app/modules/shared/components/browser-compatibility/browser-compatibility.component.spec.data.ts
+++ src/app/client/src/app/modules/shared/components/browser-compatibility/browser-compatibility.component.spec.data.ts
@@ -1,4 +1,4 @@
-export const Response = {
+export const Response: any = {
     deviceInfoFirefox: {
         'userAgent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36',
         'os': 'Linux',
