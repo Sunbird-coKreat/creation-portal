f6cc361f51b0a0349e12995fb25c068ae11168a1
Shubham Bansal <shubhambansal@TI-MAC-070.local>
2024-03-14 17:27:39 +0530

    Issue #CO-838: New Env domian Added

--- src/app/client/src/app/modules/core/services/programs/programs.service.ts
+++ src/app/client/src/app/modules/core/services/programs/programs.service.ts
@@ -41,6 +41,8 @@ export class ProgramsService extends DataService implements CanActivate {
   public config: ConfigService;
   baseUrl: string;
   public http: HttpClient;
+  public sunbirdUlr: any;
+  public defaultRedirectUrl: any = "";
   private API_URL = this.publicDataService.post; // TODO: remove API_URL once service is deployed
   ///private _contentTypes: any[];
   //private _contentCategories: any[];
@@ -67,6 +69,12 @@ export class ProgramsService extends DataService implements CanActivate {
       super(http);
       this.config = config;
       this.baseUrl = this.config.urlConFig.URLS.CONTENT_PREFIX;//"http://localhost:6000/";//
+      this.sunbirdUlr = (<HTMLInputElement>document.getElementById('sunbirdportalurl'))
+      ? (<HTMLInputElement>document.getElementById('sunbirdportalurl')).value : window.location.origin;
+
+      if(!!this.sunbirdUlr){
+        this.defaultRedirectUrl = new URL(this.sunbirdUlr);
+      }
     }
 
   /**
@@ -1240,8 +1248,7 @@ export class ProgramsService extends DataService implements CanActivate {
       case 'dockstaging.sunbirded.org': return 'https://staging.sunbirded.org'; break;
       case 'vdn.diksha.gov.in': return 'https://diksha.gov.in'; break;
       case 'dock.preprod.ntp.net.in': return 'https://preprod.ntp.net.in'; break;
-      case 'ckoci.sunbirded.org': return 'https://hotfix.sunbirded.org'; break;
-      default: return  'https://dev.sunbirded.org'; break;
+      default: return  this.defaultRedirectUrl.origin; break;
     }
   }
 
